788c9c01991af37fe39bac1385b3b7668d51fd37
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-10 17:44:23 +0100

    Coding Exercise4

---
+++ JAVA/LeapYearCalculator/src/LeapYear.java
@@ -0,0 +1,13 @@
+public class LeapYear {
+    public static boolean isLeadYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+
+        }else {
+            final boolean b = year % 100 == 0 && year % 400 == 0;
+            return b;
+        }
+    }
+}
---
+++ JAVA/LeapYearCalculator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        var result = LeapYear.isLeadYear(-1600);
+        System.out.println(result);
+    }
+}
