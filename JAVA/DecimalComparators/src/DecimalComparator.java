f7cfd73c9f23c5eba3cf2d01b5104f668ea83f40
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-10 23:41:57 +0100

    Coding Exercise5

--- JAVA/DecimalComparators/src/DecimalComparator.java
+++ JAVA/DecimalComparators/src/DecimalComparator.java
@@ -1,5 +1,10 @@
+
+
 public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo){
+
+        // bdLon = bdLon.setScale(4, BigDecimal.ROUND_HALF_UP);
+        // numberOne = numberOne.setScale(4, BigDecimal.RO)
         if (numberOne - numberTwo == 0){
             return true;
         }else if (numberOne < 0 || numberTwo < 0){
--- JAVA/DecimalComparators/src/Main.java
+++ JAVA/DecimalComparators/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         boolean result;
-        result = DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, 3.1757);
+        result = DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0);
         System.out.println(result);
     }
 }
