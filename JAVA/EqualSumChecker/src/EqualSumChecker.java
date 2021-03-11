5a609d24505155cf84da6221eca23afad3e4c902
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-11 16:25:48 +0100

    Coding Exercise6

---
+++ JAVA/EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,8 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int numberOne, int numberTwo, int numberThree){
+        if(numberOne + numberTwo == numberThree){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ JAVA/EqualSumChecker/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        var result = EqualSumChecker.hasEqualSum(1,-1,0);
+        System.out.println(result);
+    }
+}
