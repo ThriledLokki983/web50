8536bd05123f152121600c2d5fa2c9e44f8541f9
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-14 10:34:58 +0100

    PerfectNumber

---
+++ JAVA/PerfectNumber/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean result = PerfectNumber.isPerfectNumber(-1);
+        System.out.println(result);
+    }
+}
---
+++ JAVA/PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,15 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number){
+        if (number < 1){
+            return false;
+        }
+        int perfectNumber = 0;
+        int sum = 0;
+        for (int i = 1; i < number; i++){
+            if (number % i == 0){
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+}
