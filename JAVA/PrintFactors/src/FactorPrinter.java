94c796fa54b0e9605959231423ac3bb49258a5fd
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-14 10:25:51 +0100

    FactoryPrinter

---
+++ JAVA/PrintFactors/src/FactorPrinter.java
@@ -0,0 +1,14 @@
+public class FactorPrinter {
+    public static void printFactors(int number){
+        if (number < 1){
+            System.out.println("Invalid Value");
+        }
+        int factor = 0;
+        for (int i = 1; i <= number; i++){
+            if (number % i == 0){
+                factor = i;
+                System.out.println(factor);
+            }
+        }
+    }
+}
