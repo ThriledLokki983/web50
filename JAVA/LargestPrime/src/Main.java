61c1a15b37f6f5381eca2c63dd2ed61b28164f29
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-15 22:11:41 +0100

    LargestPrime

---
+++ JAVA/LargestPrime/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = LargestPrime.getLargestPrime(217)
+;//        int result = LargestPrime.getLargestPrime(21);
+        System.out.println(result);
+    }
+}
