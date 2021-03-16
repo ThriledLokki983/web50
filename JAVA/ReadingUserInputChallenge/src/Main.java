c563283628949b549fea78f10dabb4beebfbbe6b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-16 23:22:31 +0100

    ReadUserInputChallenge

--- JAVA/ReadingUserInputChallenge/src/Main.java
+++ JAVA/ReadingUserInputChallenge/src/Main.java
@@ -7,16 +7,20 @@ public class Main {
         int count = 1;
         int sum = 0;
         while(count < 11){
-            System.out.println("Enter number #" + count);
+            System.out.println("Enter number #" + count + ":");
             boolean hasInt = scanner.hasNextInt();
             if (hasInt){
                 int number = scanner.nextInt();
                 count++;
                 sum += number;
                 scanner.nextLine();
+            }else{
+                System.out.println("Invalid number entered");
+                break;
             }
+            scanner.close();
         }
         System.out.println();
-        System.out.println("The sum of all your numbers = " + sum);
+        System.out.println("The sum of all the " + (count-1) +  " numbers you entered = " + sum);
     }
 }
