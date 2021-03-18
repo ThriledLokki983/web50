d7c84bda36231ee8068fbf37da571d8aa4d1a8c5
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-18 22:17:17 +0100

    VipCustomer-CLASS

--- JAVA/VipCustomer- CLASS/src/com/company/Main.java
+++ JAVA/VipCustomer- CLASS/src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        VipCustomer customer = new VipCustomer();
+
+        customer.getName();
+
     }
 }
--- JAVA/VipCustomer- CLASS/src/com/company/VipCustomer.java
+++ JAVA/VipCustomer- CLASS/src/com/company/VipCustomer.java
@@ -6,13 +6,29 @@ public class VipCustomer {
     private int creditLimit;
 
     public VipCustomer() {
-        this("Gideon Nimoh", "customer@vipcustomer.nl", 6);
-        System.out.println("Enpty Constructor");
+        this("Default name", "Default email", 999);
+        System.out.println("Empty Constructor");
+    }
+
+    public VipCustomer(String email, int creditLimit) {
+        this("Gideon Nimoh", email, creditLimit);
     }
 
     public VipCustomer(String name, String email, int creditLimit) {
         this.name = name;
         this.email = email;
         this.creditLimit = creditLimit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
     }
 }
