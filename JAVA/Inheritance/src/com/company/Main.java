0f1897e450d25f89e96be7a6916326f593c12dc1
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-19 14:53:28 +0100

    Inheritance

--- JAVA/Inheritance/src/com/company/Main.java
+++ JAVA/Inheritance/src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Animal animal = new Animal("Animal", 3, 4, 5, 56);
+        System.out.println("Weight = " + animal.getName());
+
+        Dog dog = new Dog("Yorkie", 4, 5, 89, 45, 23, 45, "Long silky");
+        dog.eat();
+        System.out.println("Dog name = " + dog.getName());
     }
 }
