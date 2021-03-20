7b1ccfebc487784e30585f09e3e8e64f4dc14bd5
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 09:54:07 +0100

    CompEncapPoly

--- JAVA/OOP-2- CompositionEncapsulationPolymorphism/src/com/company/Main.java
+++ JAVA/OOP-2- CompositionEncapsulationPolymorphism/src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Dimensions dimensions = new Dimensions(25, 32, 6);
+        Case thecase = new Case("Zenbook-M1", "ASUS", "240-V", dimensions);
+
+        Monitor theMonitor = new Monitor("All Clear-HD", "ASUS", 27, new Resolution(2540, 1440));
+        Motherboard motherboard = new Motherboard("ACER", "ASUS", 4, 66, "Version 3.4.2");
+
+        PC thePC = new PC(thecase, theMonitor, motherboard);
+        thePC.getMonitor().drawPixelAt(2300, 6788, "blue");
+        thePC.getMotherboard().loadProgram("Mac-OS 2.8.9");
+        thePC.getTheCase().pressPowerButton();
+
     }
 }
