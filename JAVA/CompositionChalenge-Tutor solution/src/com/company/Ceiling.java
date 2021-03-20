10d1e0c7995c5e38ac1daf48fa65cf10ad35f524
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 11:10:21 +0100

    CompEncapPoly-challenge-TutorSolution

---
+++ JAVA/CompositionChalenge-Tutor solution/src/com/company/Ceiling.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Ceiling {
+    private int heigth;
+    private int width;
+
+    public Ceiling(int heigth, int width) {
+        this.heigth = heigth;
+        this.width = width;
+    }
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
---
+++ JAVA/CompositionChalenge-Tutor solution/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("North");
+        Wall wall4 = new Wall("South");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+    }
+}
