160224f6db06cce1c906ce0a690e2b327eb325d4
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 15:33:24 +0100

    Polymorphism

--- JAVA/Polymorphism/src/com/company/Main.java
+++ JAVA/Polymorphism/src/com/company/Main.java
@@ -10,6 +10,10 @@ class Movie{
     public String plot(){
         return "No plot here";
     }
+
+    public String getName() {
+        return name;
+    }
 }
 
 class Jaws extends Movie{
@@ -63,6 +67,19 @@ class Forgetable extends Movie{
     // No plt at all for this class
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -72,7 +89,34 @@ public class Main {
         // tasks This allows us to perform a single action in different ways
 
 
+        // create  a for loop
+        for(int i = 1; i < 11; i++){
+            Movie movie = randomMovie();
+            System.out.println("Movie #: " + i + ": " + movie.getName() + "\n" + "Plot: " + movie.plot() + "\n");
+        }
     }
 
+    public static Movie randomMovie(){
+    int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
 
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new independenceDay();
+
+            case 3:
+                return new Forgetable();
+
+            case 4:
+                return new starWars();
+
+            case 5:
+                return new MazeRunner();
+
+        }
+        return null;
+    }
 }
