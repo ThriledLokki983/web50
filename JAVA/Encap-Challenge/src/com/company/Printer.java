04cfbb162f3a57be0e902662cf5743c43dc57b05
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 14:29:11 +0100

    Encap - Challenge

---
+++ JAVA/Encap-Challenge/src/com/company/Printer.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = (tonerLevel > -1 && tonerLevel <= 100) ? this.tonerLevel : -1;
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount){
+        if (tonerAmount > 0 && tonerAmount <= 100){
+            // Write the logic here
+            if (this.tonerLevel + tonerAmount > 100){
+                return -1;
+            }
+            tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        }else{
+            return -1;
+        }
+    }
+
+    public int printPages(int pages){
+        int pagesToPrint = pages;
+        if (this.duplex){
+            pagesToPrint = (pages / 2)+(pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        return this.pagesPrinted += pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
