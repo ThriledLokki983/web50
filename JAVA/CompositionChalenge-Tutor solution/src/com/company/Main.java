335387dd0ead7941f9ec1c9760ac7b478a205f00
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 11:14:10 +0100

    CompEncapPoly-challenge-TutorSolution

--- JAVA/CompositionChalenge-Tutor solution/src/com/company/Main.java
+++ JAVA/CompositionChalenge-Tutor solution/src/com/company/Main.java
@@ -11,5 +11,15 @@ public class Main {
 
         Ceiling ceiling = new Ceiling(12, 55);
 
+        Bed bed = new Bed("Modern", 8, 45, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 89);
+
+        BedRoom bedRoom = new BedRoom("Gideon", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
+        bedRoom.makeBed();
+        bedRoom.getLamp().turnOn();
+
+
     }
 }
