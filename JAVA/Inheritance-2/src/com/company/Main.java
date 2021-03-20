b4a79bececc989f5d00f02815cdeec42c2956bb2
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 07:50:21 +0100

    Inheritance-2

--- JAVA/Inheritance-2/src/com/company/Main.java
+++ JAVA/Inheritance-2/src/com/company/Main.java
@@ -18,4 +18,17 @@ public class Main {
         toyota.moveCar();
         toyota.move();
     }
+
+    // METHOD OVERLOADING
+    // Providing two or more separate methods in a class with the (same name) but (different parameters)
+    // Return type may or may not be different
+    // Happens inside a single class
+
+    // METHOD OVERRIDING
+    // defining a method in a child class that already exist in the parent class with the same signature (same name & args/parameters)
+    // put @Override immediately above the method definition . Static methods can't be overridden, only instances
+    // Only inherited methods can be overridden. Thus, overriding can only happen in child classes
+    // Constructors and private methods cannot be overridden
+    // Methods that are final cannot be overridden
+    // A subclass can use super.methodName() to call the super class version of an overridden method
 }
--- JAVA/Inheritance-2/src/com/company/Vehicle.java
+++ JAVA/Inheritance-2/src/com/company/Vehicle.java
@@ -37,5 +37,30 @@ public class Vehicle {
         return energyType;
     }
 
+    // SUPER() => use to access/call the parent class members (variables & methods) -- commonly used in method overriding when
+    // we call a method with the same name from a parent class (if not we run into recursive call)
+    // THIS() => use to call the current class members (variables & members) -- commonly used constructors & Setters and
+    // optionally used in getters
+    // This is required when we have a parameter with the same name as an instance variable(field)
 
+    // NOTE!!!
+    // both can be used anywhere in a CLASS except STATIC areas (the static block or a static method).
+    // Any attempt to do so will lead to compile-time errors
+
+    // THIS()
+    // to call a constructor from another overloaded constructor in the same class
+    // It can only be used in a constructor and must be the first statement in a constructor.
+    // It is used with constructor chaining / when one constructor calls another constructor, and helps to reduce duplicated code
+
+
+    // SUPER()
+    // The only way to call a parent constructor is by calling super(). This calls the parent constructor
+    // Super() is called by the compiler by default if we don'' add it, and it is always the non-args super
+    // which is inserted by the compiler
+    // the super() must be the first statement in each constructor
+    // An Abstract class is a super class, so its constructor can run when someone makes an instance of a concrete subclass
+    //
+
+    // NOTE!!!
+    // A constructor can have a call to super() or this() but never both
 }
