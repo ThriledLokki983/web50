2317010ea0761dc13938cec127204915580ce798
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 08:35:59 +0100

    Inheritance-Challenge-1

--- JAVA/InheriatnceChallenge-1/src/com/company/Pegeout.java
+++ JAVA/InheriatnceChallenge-1/src/com/company/Pegeout.java
@@ -1,4 +1,10 @@
 package com.company;
 
-public class Pegeout {
+public class Pegeout extends Car {
+    private int roadService;
+
+    public Pegeout(int roadService) {
+        super("Pegeout", "4WD", 4, 5, 6, false);
+        this.roadService = roadService;
+    }
 }
