971c9471aaa1817417388c9abe483214c2076758
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 13:00:24 +0100

    Composition-Challenge-Real

---
+++ JAVA/Encapsulation/src/com/company/EnhancedPlayer.java
@@ -0,0 +1,30 @@
+package com.company;
+
+// Encapsulation is the mechanism of restricting access to the components of objects being created
+// Protect the members of a class from external access , thus, guarding against unauthorized access
+// It allows to change change things without breaking our code
+
+public class EnhancedPlayer {
+    private String fullName;
+    private int hitPoints = 100;
+    private String weapon;
+
+    public EnhancedPlayer(String fullName, int health, String weapon) {
+        this.fullName = fullName;
+        this.hitPoints = (health > 0 && health <= 100) ? health : this.hitPoints;
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage){
+        this.hitPoints -= damage;
+        //System.out.println("Player knocked out ") = (this.health < 0) ? this.health : ;
+        if (this.hitPoints < 0){
+            System.out.println("Player knocked out!!");
+            // reduce number of lives for current player
+        }
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+}
--- JAVA/Encapsulation/src/com/company/Main.java
+++ JAVA/Encapsulation/src/com/company/Main.java
@@ -6,20 +6,23 @@ public class Main {
 	// Encapsulation is the mechanism of restricting access to the components of objects being created
         // Protect the members of a class from external access , thus, guarding against unauthorized access
         // It allows to change change things without breaking our code
+//
+//        Player player = new Player();
+//        player.name = "Gideon";
+//        player.health = 20;
+//        player.weapon = "Gun";
+//
+//        int damage = 8;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health = " + player.healthRemaining());
+//
+//        damage = 15;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health = " + player.healthRemaining());
 
-        Player player = new Player();
-        player.name = "Gideon";
-        player.health = 20;
-        player.weapon = "Gun";
-
-        int damage = 8;
-        player.loseHealth(damage);
-        System.out.println("Remaining Health = " + player.healthRemaining());
-
-        damage = 15;
-        player.loseHealth(damage);
-        System.out.println("Remaining Health = " + player.healthRemaining());
-
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 70, "Sword");
+        System.out.println("Initial Health = " + player.getHitPoints());
 
     }
 }
