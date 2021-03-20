51fd5f75e7df636a1aef5def2bc79de06e0594e0
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-20 11:50:59 +0100

    Encapsulation

---
+++ JAVA/Encapsulation/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Encapsulation is the mechanism of restricting access to the components of objects being created
+        // Protect the members of a class from external access , thus, guarding against unauthorized access
+        // It allows to change change things without breaking our code
+
+        Player player = new Player();
+        player.name = "Gideon";
+        player.health = 20;
+        player.weapon = "Gun";
+
+        int damage = 8;
+        player.loseHealth(damage);
+        System.out.println("Remaining Health = " + player.healthRemaining());
+
+
+    }
+}
---
+++ JAVA/Encapsulation/src/com/company/Player.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Player {
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage){
+        this.health -= damage;
+        //this.health == 0 ? 0 : System.out.println("Player knocked out");
+        if (this.health < 0){
+            System.out.println("Player knocked out");
+            // reduce number of lives for current player
+        }
+    }
+
+    public int healthRemaining(){
+        return this.health;
+    }
+}
