1b1d3f9583f11cd0a3eb0b0455bc2857c7f46e5c
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-21 18:52:58 +0100

    Array List-MobilePhone Challenge

---
+++ JAVA/MobilePhone-Challenge/src/com/company/Contacts.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Contacts {
+    private ArrayList<String> contactName = new ArrayList<>();
+    private ArrayList<String> contactNumber = new ArrayList<>();
+
+
+
+
+
+
+}
---
+++ JAVA/MobilePhone-Challenge/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
