e7e95d155bf3c25322a96b9209805c60808f043b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-21 23:24:26 +0100

    Array List-MobilePhone Challenge

--- JAVA/MobilePhone-Challenge/src/com/company/Contacts.java
+++ JAVA/MobilePhone-Challenge/src/com/company/Contacts.java
@@ -6,6 +6,31 @@ public class Contacts {
     private ArrayList<String> contactName = new ArrayList<>();
     private ArrayList<String> contactNumber = new ArrayList<>();
 
+    public ArrayList<String> getContactName() {
+        return contactName;
+    }
+
+    public ArrayList<String> getContactNumber() {
+        return contactNumber;
+    }
+
+    public void addName(String name){
+        contactName.add(name);
+    }
+
+    public void addNumber(String number){
+        contactNumber.add(number);
+    }
+
+    public void printAllContacts(){
+        System.out.println("You have " + contactName.size() + " names in your contact list, and " + contactNumber.size() + " numbers");
+        for (int i = 0; i < contactName.size(); i++){
+            System.out.print((i+1) + ". " + contactName.get(i));
+            System.out.print( " " + contactNumber.get(i));
+            System.out.println();
+        }
+    }
+
 
 
 
--- JAVA/MobilePhone-Challenge/src/com/company/Main.java
+++ JAVA/MobilePhone-Challenge/src/com/company/Main.java
@@ -5,8 +5,52 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static Contacts contacts = new Contacts();
 
     public static void main(String[] args) {
 	// write your code here
+        // Use similar ideology from past studies
+
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while(!quit){
+            System.out.println("Enter you choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    addContactInfo();
+                    break;
+                case 2:
+                    contacts.printAllContacts();
+                    break;
+                case 3:
+                    quit = true;
+                    break;
+            }
+        }
+
     }
+
+    public static void printInstructions(){
+        System.out.println("=========== Please select an option: =============");
+        System.out.print("\n0 - To print choice options.");
+        System.out.print("\n1 - To add new contact information.");
+        System.out.print("\n2 - To print a all contacts.");
+        System.out.println("\n3 - To close application.");
+        System.out.println();
+        System.out.println("=================================================");
+    }
+
+    public static void addContactInfo(){
+        System.out.print("Contact Name: ");
+        contacts.addName(scanner.nextLine());
+        System.out.print("Contact Number: ");
+        contacts.addNumber(scanner.nextLine());
+    }
+
 }
