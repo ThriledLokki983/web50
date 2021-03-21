e2481a6d5486b01cd3953cf36a1c3617747ba5a0
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-21 07:54:10 +0100

    OOP -Part1a-done

---
+++ JAVA/BurgerApp Challenge/src/com/company/DeluxeBurger.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class DeluxeBurger extends Hamburger{
+
+    public DeluxeBurger() {
+        super("Deluxe Burger", "Truffle Bread", "Nantwi Nam", 25);
+    }
+
+    @Override
+    public void addToppings(int choice) {
+        super.addToppings(choice);
+        switch (choice){
+            case 1:
+                double chipsPrice = 3.50;
+                this.setAdditionalPrice(this.getAdditionalPrice() + chipsPrice);
+                System.out.println("\nAdded: Chips");
+                break;
+
+            case 2:
+                double colaPrice = 2.90;
+                this.setAdditionalPrice(this.getAdditionalPrice() + colaPrice);
+                System.out.println("\nAdded: Cola");
+                break;
+        }
+    }
+
+    @Override
+    public void printMenu() {
+        super.printMenu();
+        System.out.println("1. Chips        || Base price: $3.50");
+        System.out.println("2. Cola         || Base price: $2.90");
+    }
+
+    @Override
+    public void checkOutOrder() {
+        super.checkOutOrder();
+    }
+}
