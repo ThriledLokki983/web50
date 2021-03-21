fdc076a7b8abbd1f7b1ba1bf5d26cedc85ad550b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-21 16:01:46 +0100

    Array List

---
+++ JAVA/LISTS/src/com/company/GroceryList.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+    }
+
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in you grocery list");
+        for (int i = 0; i < groceryList.size(); i++){
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified");
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        System.out.println("Grocery item " + (position + 1) + " has been deleted");
+    }
+
+
+}
