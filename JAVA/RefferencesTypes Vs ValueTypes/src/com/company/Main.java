4d4f0afe9ecce9cd8c676d1d04dd983a40c22f51
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-21 13:34:42 +0100

    Reference Vs Value

--- JAVA/RefferencesTypes Vs ValueTypes/src/com/company/Main.java
+++ JAVA/RefferencesTypes Vs ValueTypes/src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int myIntValue = 10;
+        int myOtherValue = myIntValue;
 
+        // Value Types
+        System.out.println("My IntValue = " + myIntValue);
+        System.out.println("My MyOtherValue = " + myOtherValue);
+
+        myOtherValue++;
+
+        System.out.println("My IntValue = " + myIntValue);
+        System.out.println("My MyOtherValue = " + myOtherValue);
+
+        // Reference Type
+        int[] myIntArray = new int[5]; // this holds an address of the object and NOT the object itself
+        int[] myOtherArray = myIntArray;
+
+        System.out.println("My IntArray " + Arrays.toString(myIntArray));
+        System.out.println("My OtherArray " + Arrays.toString(myOtherArray));
+
+        myIntArray[3] = 56;
+        System.out.println("My IntArray after change " + Arrays.toString(myIntArray));
+        System.out.println("My OtherArray after change " + Arrays.toString(myOtherArray));
     }
 }
