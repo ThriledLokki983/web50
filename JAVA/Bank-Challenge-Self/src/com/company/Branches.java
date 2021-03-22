335e1d76a199155e378fdd69331161604cf63d64
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-22 23:36:29 +0100

    Array List-Bank-self

--- JAVA/Bank-Challenge-Self/src/com/company/Branches.java
+++ JAVA/Bank-Challenge-Self/src/com/company/Branches.java
@@ -3,7 +3,23 @@ package com.company;
 import java.util.ArrayList;
 
 public class Branches {
+    private String branchName;
     private ArrayList<Customer> myBranches;
+
+
+    public Branches(String branchName) {
+        this.branchName = branchName;
+        this.myBranches = new ArrayList<>();
+    }
+
+    // Loop through my customer List if the Customer exist
+    private int findCustomer(String customerName){
+        return this.branchName.indexOf(customerName);
+    }
+    // Loop through my customer List if the Branch exist
+    private int findBranch(Customer branchName){
+        return this.myBranches.indexOf(branchName);
+    }
 
 
 }
