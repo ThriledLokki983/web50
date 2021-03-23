bb9c011078e66da961f3591e6bdfb751ae2f674d
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-23 23:20:13 +0100

    Finance-Bank

---
+++ JAVA/Finance/src/com/company/Bank.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private final String bankName;
+    private final ArrayList<Branch> myBranches;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+        this.myBranches = new ArrayList<>();
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+    public ArrayList<Branch> getMyBranches() {
+        return myBranches;
+    }
+
+    private Branch findBranch(String branchName){
+        for (Branch theBranch : myBranches) {
+            if (theBranch.getBranchName().equals(branchName)) {
+                return theBranch;
+            }
+        }
+        return null;
+    }
+
+    public boolean addBranch(String branchName){
+        if (findBranch(branchName) == null){
+            this.myBranches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double transactionAmount){
+        Branch branch = findBranch(branchName);
+        if (branch != null){
+            branch.addNewCustomer(customerName, transactionAmount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount){
+        Branch branch = findBranch(branchName);
+        if (branch != null){
+            branch.addCustomerTransaction(customerName, amount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean printCustomers(String branchName, boolean showTransaction){
+        Branch theBranch = findBranch(branchName);
+        if (theBranch != null){
+            System.out.println("Branch Name: " + theBranch.getBranchName());
+            ArrayList<Customer> branchCustomers = theBranch.getMyCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++){
+                Customer customer = branchCustomers.get(i);
+                System.out.println((i + 1)+ "Customer Name: " + customer.getCustomerName());
+                if (showTransaction){
+                    System.out.println("Transactions: ");
+                    ArrayList<Double> transaction = customer.getTransactions();
+                    for (int j = 0; j < transaction.size(); j++){
+                        System.out.println((j+1) + ". Amount: " + transaction.get(j));
+                    }
+                }
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
--- JAVA/Finance/src/com/company/Main.java
+++ JAVA/Finance/src/com/company/Main.java
@@ -9,7 +9,61 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while(!quit){
+            System.out.print("Select an Option: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    branchInstructions();
+                    break;
+                case 2:
+                    quit = true;
+                    break;
+            }
+        }
+    }
 
+    public static void printInstructions(){
+        System.out.print("=========== Please select an option: ============");
+        System.out.print("\n0 - Instructions.");
+        System.out.print("\n1 - Create Bank Account.");
+        System.out.println("\n2 - Quit!.");
+        System.out.println("=================================================");
+
+    }
+
+    public static void selectOption(){
+        System.out.print("=========== Please select an option: ============");
+        System.out.print("\n0 - Instructions.");
+        System.out.print("\n1 - Create Bank Account.");
+        System.out.println("\n2 - Quit!.");
+        System.out.println("=================================================");
+
+    }
+
+    public static void branchInstructions() {
+        boolean quit = false;
+        int choice = 0;
+        selectOption();
+        while (!quit) {
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    modifyContactName();
+                    quit = true;
+                case 2:
+                    quit = true;
+                    break;
+            }
+        }
     }
 
 
