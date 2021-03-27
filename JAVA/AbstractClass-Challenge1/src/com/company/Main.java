b4fcf162dc1a3cde2b9710e90e0a118ec4dbbb23
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-27 21:09:31 +0100

    Abstract Classes-Challenge TS

--- JAVA/AbstractClass-Challenge1/src/com/company/Main.java
+++ JAVA/AbstractClass-Challenge1/src/com/company/Main.java
@@ -6,8 +6,10 @@ public class Main {
 	// write your code here
 
         String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        /*
         String theData = "9 3 8 6 7 4 1 2 0 9 6 4 5 7";
         String myStringData = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.";
+       */
 
         SearchTree tree = new SearchTree(null);
         tree.traverse(tree.getRoot());
