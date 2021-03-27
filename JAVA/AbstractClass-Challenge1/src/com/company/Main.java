f46b520c9ef86261e44ac01713efc96d9d223270
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-27 18:24:40 +0100

    Abstract Classes-Challenge TS

--- JAVA/AbstractClass-Challenge1/src/com/company/Main.java
+++ JAVA/AbstractClass-Challenge1/src/com/company/Main.java
@@ -6,11 +6,13 @@ public class Main {
 	// write your code here
 
         String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String theData = "9 3 8 6 7 4 1 2 0 9 6 4 5 7";
+        String myStringData = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.";
 
         MyLinkedList list = new MyLinkedList(null);
         //list.traverse(list.getRoot());
 
-        String[] data = stringData.split(" ");
+        String[] data = theData.split(" ");
         for (String s : data){
             list.addItem(new Node(s));
         }
--- JAVA/AbstractClass-Challenge1/src/com/company/MyLinkedList.java
+++ JAVA/AbstractClass-Challenge1/src/com/company/MyLinkedList.java
@@ -65,6 +65,32 @@ public class MyLinkedList implements NodeList{
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null){
+            System.out.println("Deleting item: " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        while(currentItem != null){
+            int comparison = currentItem.compareTo(item);
+            if (comparison == 0){
+                if (currentItem == this.root){
+                    this.root = currentItem.next();
+                }else{
+                    currentItem.previous().setNext(currentItem.next());
+                    if (currentItem.next() != null){
+                        currentItem.setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            }else if (comparison < 0){
+                currentItem = currentItem.next();
+            }else{// comparison > 0
+                // We are at the item greater than the one to be deleted
+                // so the item is not in the list
+                return false;
+            }
+        }
+        // We have reached the end of the list
+        // Without finding the item to be deleted
         return false;
     }
 
