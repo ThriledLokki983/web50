663d510f4e7482707f7b4dd2264065fb8d59daf4
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-27 19:11:05 +0100

    Abstract Classes-Challenge TS

--- JAVA/AbstractClass-Challenge1/src/com/company/Main.java
+++ JAVA/AbstractClass-Challenge1/src/com/company/Main.java
@@ -9,13 +9,35 @@ public class Main {
         String theData = "9 3 8 6 7 4 1 2 0 9 6 4 5 7";
         String myStringData = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.";
 
-        MyLinkedList list = new MyLinkedList(null);
-        //list.traverse(list.getRoot());
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
 
-        String[] data = theData.split(" ");
+        String[] data = stringData.split(" ");
         for (String s : data){
-            list.addItem(new Node(s));
+            tree.addItem(new Node(s));
         }
-        list.traverse(list.getRoot());
+        //tree.traverse(tree.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("Darwin"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("Adelaide"));
+        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("ipsum"));
+//        tree.removeItem(new Node("used"));
+//        tree.removeItem(new Node("placeholder"));
+        tree.traverse(tree.getRoot());
+
+//        tree.removeItem(new Node("is"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("a"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("a"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
     }
 }
--- JAVA/AbstractClass-Challenge1/src/com/company/SearchTree.java
+++ JAVA/AbstractClass-Challenge1/src/com/company/SearchTree.java
@@ -22,13 +22,114 @@ public class SearchTree implements NodeList{
     }
 
     @Override
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
         // Implementing a Binary Search tree
+        if (this.root == null){
+            this.root = newItem;
+            return true;
+        }
+        ListItem currentItem = this.root;
+        while (currentItem != null){
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0){
+                // newItem is greater than 0, move right if possible
+                if (currentItem.next() != null){
+                    currentItem = currentItem.next();
+                }else{
+                    // there's no node to the right, so add this point
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+            }else if (comparison > 0){
+                // newItem is less, move left if possible
+                if (currentItem.previous() != null){
+                    currentItem = currentItem.previous();
+                }else{
+                    currentItem.setPrevious(newItem);
+                    return true;
+                }
+            }else{
+                // equal, so do not add
+                System.out.println(newItem.getValue() + " is already present");
+                return false;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        ListItem parentItem = currentItem;
+
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(item));
+            if (comparison < 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.next();
+            } else if (comparison > 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.previous();
+            } else {
+                // equal: we've found the item so remove it
+                performRemoval(currentItem, parentItem);
+                return true;
+            }
+        }
         return false;
+    }
+
+    private void performRemoval(ListItem item, ListItem parent) {
+        // remove item from the tree
+        if (item.next() == null) {
+            // no right tree, so make parent point to left tree (which may be null)
+            if (parent.next() == item) {
+                // item is right child of its parent
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                // item is left child of its parent
+                parent.setPrevious(item.previous());
+            } else {
+                // parent must be item, which means we were looking at the root of the tree
+                this.root = item.previous();
+            }
+        } else if (item.previous() == null) {
+            // no left tree, so make parent point to right tree (which may be null)
+            if (parent.next() == item) {
+                // item is right child of its parent
+                parent.setNext(item.next());
+            } else if (parent.previous() == item) {
+                // item is left child of its parent
+                parent.setPrevious(item.next());
+            } else {
+                // again, we are deleting the root
+                this.root = item.next();
+            }
+        } else {
+            // neither left nor right are null, deletion is now a lot trickier!
+            // From the right sub-tree, find the smallest value (i.e., the leftmost).
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while (current.previous() != null) {
+                leftmostParent = current;
+                current = current.previous();
+            }
+            // Now put the smallest value into our node to be deleted
+            item.setValue(current.getValue());
+            // and delete the smallest
+            if (leftmostParent == item) {
+                // there was no leftmost node, so 'current' points to the smallest
+                // node (the one that must now be deleted).
+                item.setNext(current.next());
+            } else {
+                // set the smallest node's parent to point to
+                // the smallest node's right child (which may be null).
+                leftmostParent.setPrevious(current.next());
+            }
+        }
     }
 
     @Override
@@ -39,5 +140,9 @@ public class SearchTree implements NodeList{
             System.out.println(root.getValue());
             traverse(root.next());
         }
+//        if (root != null){
+//            System.out.println(root.getValue());
+//            traverse(root.next());
+//        }
     }
 }
