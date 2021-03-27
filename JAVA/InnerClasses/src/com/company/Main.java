1535a19af0b67f03d26a1c9f4c3ed702f3363ffc
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-27 12:32:36 +0100

    InnerClasses

--- JAVA/InnerClasses/src/com/company/Main.java
+++ JAVA/InnerClasses/src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Gearbox mcLaren = new Gearbox(6);
+        mcLaren.addGear(1, 5.3);
+        mcLaren.addGear(2, 10.6);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+//        Gearbox.Gear first = mcLaren.new Gear(1, 12.4);
+//        System.out.println(first.driveSpeed(1000));
     }
 }
