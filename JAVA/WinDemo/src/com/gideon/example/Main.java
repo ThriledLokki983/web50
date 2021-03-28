0e9bd6ea29d9c603147ffaedf801a034102f8088
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-28 20:56:28 +0200

    WindowDemo

---
+++ JAVA/WinDemo/src/com/gideon/example/Main.java
@@ -0,0 +1,10 @@
+package com.gideon.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
--- JAVA/WinDemo/src/com/gideon/example/MyWindow.java
+++ JAVA/WinDemo/src/com/gideon/example/MyWindow.java
@@ -1,11 +1,38 @@
 package com.gideon.example;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
- * Name: Gideon Nimoh 
+ * Name: Gideon Nimoh
  * Date: 3/28/21
  * Time: 11:56 AM
  * To change this template use File | Settings | File and Code Templates.
  */
 
 
-public class MyWindow {
+public class MyWindow extends Frame {
+
+    public MyWindow(String title){
+        super(title);
+        setSize(500, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("The Complete Java Developer Course", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("by: Gideon Nimoh", 60, 100);
+    }
 }
