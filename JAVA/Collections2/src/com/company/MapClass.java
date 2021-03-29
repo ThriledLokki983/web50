447d6607cbecba8e5f26aae777167cacb66c456e
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-03-29 20:24:08 +0200

    Collections Maps

---
+++ JAVA/Collections2/src/com/company/MapClass.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Name: Gideon Nimoh
+ * Date: 3/29/21
+ * Time: 7:59 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+
+
+public class MapClass {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "Compiled high level, object-oriented, platform independent language");
+        languages.put("Python", "Compiled high level, object-oriented, programming language");
+        languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code");
+        languages.put("Lisp", "Therein lies madness");
+        languages.put("BASIC", "Lets see if you will be overwritten");
+
+        if (languages.containsKey("BASIC")){
+            System.out.println("It already exist");
+        }else{
+            System.out.println(languages.get("BASIC"));
+        }
+
+        System.out.println("==========================================================================");
+        for (String key : languages.keySet()){
+            System.out.println(key + " : " + languages.get(key));
+        }
+    }
+
+}
