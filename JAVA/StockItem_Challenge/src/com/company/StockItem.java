a80432b92c543fedf9eb20919bbf686dcc69a8f3
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-01 15:05:15 +0200

    StockItem_Challenge

---
+++ JAVA/StockItem_Challenge/src/com/company/StockItem.java
@@ -0,0 +1,85 @@
+package com.company;
+
+/**
+ * Name: Gideon Nimoh
+ * Date: 4/1/21
+ * Time: 2:45 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+
+
+public class StockItem implements Comparable<StockItem>{
+    private final String name;
+    private double price;
+    private int quantityStock;
+    private int reserved = 0;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int quantityInStock() {
+        return quantityStock - reserved;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0.0) this.price = price;
+    }
+
+    public void setQuantityStock(int quantityStock) {
+        this.quantityStock = quantityStock;
+    }
+
+    public void adjustStock(int quantity){
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0 ) this.quantityStock = newQuantity;
+    }
+
+    public void reserveStock(int quantity){
+        int reservedStock = this.reserved + quantity;
+        if (reservedStock >= 0) this.reserved = quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals(): ");
+        if (obj == this) return true;
+        if ((obj == null) || (obj.getClass() != this.getClass())) return false;
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Entering StockItem.CompareTo(): ");
+        if (this == o) return 0;
+        if (o != null) return this.name.compareTo(o.getName());
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.price + ": " + this.reserved;
+    }
+}
---
+++ JAVA/StockItem_Challenge/src/com/company/StockList.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Name: Gideon Nimoh
+ * Date: 4/1/21
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+
+    public StockList() {
+        this.list = new LinkedHashMap<>();
+    }
+
+    public int addItem(StockItem item){
+        if (item != null){
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            //StockItem inStock = list.get(item.getName());
+            // check if quantity is already > 0
+            if (inStock != item){
+                item.adjustStock(inStock.quantityInStock());
+            }
+            list.put(item.getName(), item);
+            return item.quantityInStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity){
+        StockItem inStock = list.getOrDefault(item, null);
+        if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)){
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public int reserveItem(String item, int quantity){
+        StockItem inStock = list.getOrDefault(item, null);
+        int reservedItems = 0;
+        if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity >0)){
+            inStock.reserveStock(quantity);
+            return reservedItems;
+        }
+        return 0;
+    }
+
+    public StockItem get(String key){
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items(){
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("\nStock List\n");
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item : list.entrySet()){
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            s.append(stockItem);
+            s.append(". There are ");
+            s.append(stockItem.quantityInStock());
+            s.append(" in stock. Value of items: \t");
+            s.append(String.format("%.2f", itemValue)).append("\n");
+            totalCost += itemValue;
+        }
+        return s + "Total stock value " + String.format("%.2f",totalCost);
+    }
+}
