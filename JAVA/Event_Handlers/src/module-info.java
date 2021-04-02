1bb50893732b92be407807e1368e52ff0e79c3b4
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-02 11:01:45 +0200

    JAVA-FX_Control_App

---
+++ JAVA/Event_Handlers/src/module-info.java
@@ -0,0 +1,6 @@
+module Event.Handlers {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ JAVA/Event_Handlers/src/sample/Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Controller {
+    // MVC - Model View Controller
+
+    public void onButtonClick(){
+        System.out.println("Hello, Gideon!");
+    }
+}
