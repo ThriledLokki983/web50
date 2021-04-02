4cae6b69c145ce365d7196509d8c8a64982f59b8
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-02 11:40:30 +0200

    JAVA-FX_Control_App

--- JAVA/Event_Handlers/src/sample/Controller.java
+++ JAVA/Event_Handlers/src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -13,6 +14,13 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox ourCheckBox;
+
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
 
     @FXML
     public void onButtonClick(ActionEvent e){
@@ -21,5 +29,22 @@ public class Controller {
         }else if (e.getSource().equals(byeButton)){
             System.out.println("Bye bye, " + nameField.getText());
         }
+        if (ourCheckBox.isSelected()){
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    public void handleKeyRelease(){
+        String text = nameField.getText();
+        boolean disableButton = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButton);
+        byeButton.setDisable(disableButton);
+    }
+    @FXML
+    public void handleChange(){
+        System.out.println("The Checkbox is :" + (ourCheckBox.isSelected() ? " checked" : " not checked"));
     }
 }
