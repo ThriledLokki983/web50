46a091327709b14537bcc9d3540843df449d325b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-03 23:51:42 +0200

    JAVA-FX_ Challenge-Self

--- JAVA/CL_Application/src/sample/Controller.java
+++ JAVA/CL_Application/src/sample/Controller.java
@@ -5,14 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
 public class Controller {
-    @FXML
-    private TableView<Contact> myContacts;
+    private TableView<Contact> myContacts = new TableView<>();
+
 
 
 
 
 
     public void initialize(){
+        myContacts item1 =  new Contact("Gideon", "Nimoh", "0626603494", "This is just a test note");
 
     }
 
