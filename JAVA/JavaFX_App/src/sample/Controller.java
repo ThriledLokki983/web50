7ebc0466e9cd0d8d77f5d7f1af46f3316f5613dc
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-03 14:14:19 +0200

    JAVA-FX_ JavaFx_Application-CSS

--- JAVA/JavaFX_App/src/sample/Controller.java
+++ JAVA/JavaFX_App/src/sample/Controller.java
@@ -1,5 +1,46 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+
 public class Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private GridPane gridPane;
+
+
+    public void initialize(){
+        button4.setEffect(new DropShadow());
+
+    }
+
+    @FXML
+    public void handleMouseEnter(){
+        label.setScaleY(2.0);
+        label.setScaleX(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){
+        label.setScaleY(1.0);
+        label.setScaleX(1.0);
+    }
+
+    @FXML
+    public void handleClick(){
+        FileChooser chooser = new FileChooser();
+        chooser.showOpenDialog(gridPane.getScene().getWindow());
+    }
+
 
 }
