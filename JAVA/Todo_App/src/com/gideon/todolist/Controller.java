18d88c1657a6ce363a99da86b1a35a4e03b49b8a
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-03 11:27:55 +0200

    JAVA-FX_Todo_List

--- JAVA/Todo_App/src/com/gideon/todolist/Controller.java
+++ JAVA/Todo_App/src/com/gideon/todolist/Controller.java
@@ -9,6 +9,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -146,6 +148,16 @@ public class Controller {
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResult();
             todoListView.getSelectionModel().select(newItem);
+        }
+    }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent){
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null){
+            if (keyEvent.getCode().equals(KeyCode.DELETE)){
+                deleteItem(selectedItem);
+            }
         }
     }
 
