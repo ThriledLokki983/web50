884bb1f0c2ebac2c5c9d5fb315dacc2e2ee71001
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-04 22:11:55 +0200

    InputOutput-Correct

---
+++ JAVA/InputOutput/src/com/company/Location.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Name: Gideon Nimoh
+ * Date: 3/30/21
+ * Time: 2:43 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+
+
+public final class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description, Map<String, Integer> exits) {
+        this.locationID = locationID;
+        this.description = description;
+        if(exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
+        this.exits.put("Q", 0);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(exits);
+    }
+
+}
