70716057b648512cadba594e179dd42426149170
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-04 14:49:53 +0200

    InputOutput

--- JAVA/InputIO/src/com/company/Locations.java
+++ JAVA/InputIO/src/com/company/Locations.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -16,11 +17,21 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locationMap = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        FileWriter locFile = null;
+        try(FileWriter locFile = new FileWriter("locations.txt");
+        FileWriter dirFile = new FileWriter("directions.txt")){
+            for (Location location : locationMap.values()){
+                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+                for (String direction : location.getExits().keySet()){
+                    dirFile.write(location.getLocationID() + ", " + direction + ", " + location.getExits().get(direction) + "\n");
+                }
+            }
+        }
+/*        FileWriter locFile = null;
         try{
             locFile = new FileWriter("locations.txt");
             for (Location location : locationMap.values()){
                 locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+                //throw new IOException("Test exception while writing");
             }
         }finally {
             System.out.println("In finally block");
@@ -28,11 +39,31 @@ public class Locations implements Map<Integer, Location> {
                 System.out.println("Attempting to close Loc file");
                 locFile.close();
             }
-        }
+        }*/
     }
 
     static {
-        Map<String, Integer> tempExit = new HashMap<>();
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(new FileReader("locations.txt"));
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()){
+                int loc = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String description = scanner.nextLine();
+                System.out.println("Imported Loc: " + loc + ": " + description);
+                Map<String, Integer> tempExit = new HashMap<>();
+                locationMap.put(loc, new Location(loc, description, tempExit));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (scanner != null){
+                scanner.close();
+            }
+        }
+
+        /*Map<String, Integer> tempExit = new HashMap<>();
         locationMap.put(0, new Location(0, "You are sitting in front of a computer learning java",tempExit));
 
         tempExit = new HashMap<>();
@@ -58,7 +89,7 @@ public class Locations implements Map<Integer, Location> {
         tempExit = new HashMap<>();
         tempExit.put("S", 1);
         tempExit.put("W", 2);
-        locationMap.put(5, new Location(5, "You are in the forest",tempExit));
+        locationMap.put(5, new Location(5, "You are in the forest",tempExit));*/
 
     }
 
