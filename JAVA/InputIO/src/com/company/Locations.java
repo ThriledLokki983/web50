12f9db4e4d13bf78b00f34f08f981249bc72b911
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-04 13:25:58 +0200

    InputOutput

--- JAVA/InputIO/src/com/company/Locations.java
+++ JAVA/InputIO/src/com/company/Locations.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -12,6 +14,30 @@ import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locationMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try{
+            locFile = new FileWriter("locations.txt");
+            for (Location location : locationMap.values()){
+                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+            }
+        }catch (IOException e){
+            System.out.println("In catch block");
+            e.printStackTrace();
+        }finally {
+            System.out.println("In finally block");
+            try{
+                if (locFile != null){
+                    System.out.println("Attempting to close Loc file");
+                    locFile.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     static {
         Map<String, Integer> tempExit = new HashMap<>();
