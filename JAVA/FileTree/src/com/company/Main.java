723a47d8bd86512be0bbefc8aacac31149cf0cd5
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-05 19:17:38 +0200

    FileTree

--- JAVA/FileTree/src/com/company/Main.java
+++ JAVA/FileTree/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -17,7 +18,8 @@ public class Main {
 
         DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
 
-        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
+       // Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
+        Path directory = FileSystems.getDefault().getPath("FileTree"+ File.separator + "Dir2");
 
         try(DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)){
                 for (Path file : contents){
@@ -25,6 +27,30 @@ public class Main {
                 }
         }catch (IOException | DirectoryIteratorException e){
             System.out.println(e.getMessage());
+        }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+
+        try{
+            Path tempFile = Files.createTempFile("myapp", ".appext");
+            System.out.println("Temporary File path: " + tempFile.toAbsolutePath());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for (FileStore store : stores){
+            System.out.println("Volume name/Drive Letter : " + store);
+            System.out.println(store.name());
+        }
+
+        System.out.println("=============================");
+        Iterable<Path> rootPath = FileSystems.getDefault().getRootDirectories();
+        for (Path path : rootPath){
+            System.out.println(path);
         }
     }
 }
