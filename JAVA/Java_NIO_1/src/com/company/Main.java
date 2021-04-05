35db3270d9dda30f310bf71784b5024c25d97c98
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-05 13:47:04 +0200

    JAVA_NIO

--- JAVA/Java_NIO_1/src/com/company/Main.java
+++ JAVA/Java_NIO_1/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try{
+           /* FileInputStream file = new FileInputStream("data.txt");
+            FileChannel channel = file.getChannel();*/
+
+            Path path = FileSystems.getDefault().getPath("data.txt");
+            Files.write(path, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
+            List<String> lines = Files.readAllLines(path);
+            for (String s : lines){
+                System.out.println(s);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
