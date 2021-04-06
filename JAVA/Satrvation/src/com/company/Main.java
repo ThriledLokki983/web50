a57d27d2578be27ba4c29ce8fc5f32571c40f769
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-06 20:44:37 +0200

    Starvation

--- JAVA/Satrvation/src/com/company/Main.java
+++ JAVA/Satrvation/src/com/company/Main.java
@@ -4,7 +4,26 @@ public class Main {
     private static Object lock = new Object();
 
     public static void main(String[] args) {
-	// write your code here
+	 Thread t1 = new Thread(new Worker(ThreadColor.ANSI_YELLOW), "Priority 10");
+        Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 8");
+        Thread t3 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 6");
+        Thread t4 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "Priority 4");
+        Thread t5 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 2");
+
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+
+
     }
 
     private static class Worker implements Runnable{
@@ -25,6 +44,8 @@ public class Main {
             }
         }
     }
+
+
 
 
 }
