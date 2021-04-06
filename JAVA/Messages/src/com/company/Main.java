108d16872f69d70a36404acd411d48fdbe32b143
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-06 18:02:13 +0200

    Thread-Messages

---
+++ JAVA/Messages/src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class Message{
+    private String message;
+    private boolean empty;
+
+    public synchronized String read(){
+        while (empty){
+
+        }
+        empty = true;
+        return message;
+    }
+
+    public synchronized  void write(String message){
+        while (!empty){
+
+        }
+        empty = false;
+        this.message = message;
+    }
+}
+
+class Writer implements Runnable{
+    private Message message;
+
+    public Writer(Message message){
+        this.message = message;
+    }
+
+    public void run(){
+        String messages[] = {
+                "Humpty Dumpty sat on the wall",
+                "Humpty Dumpty had a great fall",
+                "All the king's horses and all the king's men",
+                "Couldn't put Humpty together again"
+        };
+        Random random = new Random();
+        for (int i = 0; i < messages.length; i++ ){
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            }catch (InterruptedException e){
+                System.out.println(e.getCause());
+            }
+        }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable{
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run(){
+        Random random = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()){
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            }catch (InterruptedException e){
+                System.out.println(e.getCause());
+            }
+        }
+    }
+}
