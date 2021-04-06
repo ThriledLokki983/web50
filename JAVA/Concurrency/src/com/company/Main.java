5eca99722b02eadf1b15cc1ef95c58670c19de9f
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-06 19:30:37 +0200

    Producer-Consumer

--- JAVA/Concurrency/src/com/company/Main.java
+++ JAVA/Concurrency/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -12,13 +13,37 @@ public class Main {
 
         List<String> buffer = new ArrayList<>();
         ReentrantLock bufferLock = new ReentrantLock();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
 
-        new Thread(producer).start();
+        /*new Thread(producer).start();
         new Thread(consumer1).start();
-        new Thread(consumer2).start();
+        new Thread(consumer2).start();*/
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_GREEN + "This is being prined from the Callable Class ");
+                return "This is the Callable Result";
+            }
+        });
+
+        try {
+            System.out.println(ThreadColor.ANSI_GREEN + future.get());
+        }catch (ExecutionException e){
+            System.out.println(ThreadColor.ANSI_RED + "Something went wrong");
+        }catch (InterruptedException e){
+            System.out.println(ThreadColor.ANSI_RED + "Thread running the task was interrupted");
+        }
+
+        executorService.shutdown();
     }
 
 }
