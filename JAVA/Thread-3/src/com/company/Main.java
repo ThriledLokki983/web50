ebd09e97d793e5b2f5cc2d32fc9e629aa00beb43
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-06 16:45:12 +0200

    Thread-3

--- JAVA/Thread-3/src/com/company/Main.java
+++ JAVA/Thread-3/src/com/company/Main.java
@@ -6,19 +6,26 @@ public class Main {
 	// write your code here
 
         Countdown countdown = new Countdown();
+        Countdown countdown1 = new Countdown();
+
+
         CountdownThread t1 = new CountdownThread(countdown);
         t1.setName("Thread 1");
         CountdownThread t2 = new CountdownThread(countdown);
         t2.setName("Thread 2");
 
+        CountdownThread t3 = new CountdownThread(countdown);
+        t3.setName("Thread 3");
+
         t1.start();
         t2.start();
+        //t3.start();
 
     }
 }
 class Countdown{
     private int i;
-    public void doCountdown(){
+    public synchronized void doCountdown(){
         String color = null;
         switch (Thread.currentThread().getName()){
             case "Thread 1":
@@ -29,6 +36,7 @@ class Countdown{
                 break;
             case "Thread 3":
                 color = ThreadColor.ANSI_GREEN;
+                break;
         }
         for (i = 10; i > 0; i--){
             System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
