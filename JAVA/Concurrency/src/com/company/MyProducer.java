14f91ed94dfe2e63c20d2b420219654124a0239f
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-06 18:33:42 +0200

    Producer-Consumer

---
+++ JAVA/Concurrency/src/com/company/MyProducer.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Name: Gideon Nimoh
+ * Date: 4/6/21
+ * Time: 6:28 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+
+
+public class MyProducer implements Runnable{
+    private List<String> buffer;
+    private String color;
+
+    public MyProducer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String[] nums = {"1","2", "3", "4", "5"};
+
+        for (String num : nums){
+            try {
+                System.out.println(color + "Adding --- " + num);
+                buffer.add(num);
+
+                Thread.sleep(random.nextInt(1000));
+            }catch (InterruptedException e){
+                System.out.println("Producer was interrupted");
+            }
+        }
+        System.out.println(color + "Adding EOF and exiting....");
+        buffer.add("EOF");
+    }
+}
