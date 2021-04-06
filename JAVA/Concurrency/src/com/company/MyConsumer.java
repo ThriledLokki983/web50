627eb81e3466dbc9f8cde92a3c772d2e4566a9c2
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-06 19:16:53 +0200

    Producer-Consumer

--- JAVA/Concurrency/src/com/company/MyConsumer.java
+++ JAVA/Concurrency/src/com/company/MyConsumer.java
@@ -27,20 +27,26 @@ public class MyConsumer implements Runnable{
 
     @Override
     public void run() {
+        int counter = 0;
         while (true){
-                bufferLock.lock();
-                try {
-                    if (buffer.isEmpty()){
-                        continue;
+                if (bufferLock.tryLock()){
+                    try {
+                        if (buffer.isEmpty()){
+                            continue;
+                        }
+                        System.out.println(color + "The counter = " + counter);
+                        counter = 0;
+                        if (buffer.get(0).equals(EOF)){
+                            System.out.println(color + "Exiting");
+                            break;
+                        }else{
+                            System.out.println(color + "Removed " + buffer.remove(0));
+                        }
+                    }finally {
+                        bufferLock.unlock();
                     }
-                    if (buffer.get(0).equals(EOF)){
-                        System.out.println(color + "Exiting");
-                        break;
-                    }else{
-                        System.out.println(color + "Removed " + buffer.remove(0));
-                    }
-                }finally {
-                    bufferLock.unlock();
+                }else {
+                    counter++;
                 }
         }
     }
