77c3ad0786ffbadd7779805a6fa2626d393ae155
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-07 17:08:16 +0200

    Task - Data Binding

--- JAVA/TaskExample/src/sample/Controller.java
+++ JAVA/TaskExample/src/sample/Controller.java
@@ -5,7 +5,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
@@ -14,20 +16,32 @@ public class Controller {
     @FXML
     private ListView listView;
 
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
+
 
     public void initialize(){
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-                ObservableList<String> employees = FXCollections.observableArrayList(
-                        "Gideon Nimoh",
+                String[] names = {"Gideon Nimoh",
                         "Kwame Agyin",
                         "Paapa Nimoh",
                         "Joan Andrews",
                         "Elizabeth Osei",
                         "Dennis Prempeh",
-                        "Dominic McDonald");
+                        "Dominic McDonald"};
+
+                ObservableList<String> employees = FXCollections.observableArrayList();
+                for (int i = 0; i < 7; i++){
+                    employees.add(names[i]);
+                    updateMessage("Added: " + names[i]);
+                    updateProgress(i + 1, 7);
+                    Thread.sleep(200);
+                }
                 /*Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
@@ -38,6 +52,8 @@ public class Controller {
                 return employees;
             }
         };
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 
