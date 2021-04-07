75b97469af0516f1e9e6dffb85fcb573c5df8499
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-07 16:09:51 +0200

    Starvation

--- JAVA/Satrvation/src/com/company/Main.java
+++ JAVA/Satrvation/src/com/company/Main.java
@@ -1,20 +1,22 @@
 package com.company;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
-	 Thread t1 = new Thread(new Worker(ThreadColor.ANSI_YELLOW), "Priority 10");
+	    Thread t1 = new Thread(new Worker(ThreadColor.ANSI_YELLOW), "Priority 10");
         Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 8");
         Thread t3 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 6");
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 2");
 
-/*        t1.setPriority(10);
+        t1.setPriority(10);
         t2.setPriority(8);
         t3.setPriority(6);
         t4.setPriority(4);
-        t5.setPriority(2);*/
+        t5.setPriority(2);
 
         t1.start();
         t2.start();
@@ -35,15 +37,15 @@ public class Main {
         @Override
         public void run() {
             for (int i=0; i<100; i++){
-                synchronized (lock){
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
                     // execute critical section of code
+                }finally {
+                    lock.unlock();
                 }
             }
         }
     }
-
-
-
 
 }
