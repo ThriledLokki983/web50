f6694d8708df854de9c78b3d479e22820e212073
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-07 18:59:39 +0200

    Thread-Challenge

--- JAVA/COncurrencyChallenge/src/com/company/BankAccount.java
+++ JAVA/COncurrencyChallenge/src/com/company/BankAccount.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Name: Gideon Nimoh
  * Date: 4/7/21
@@ -11,21 +14,64 @@ package com.company;
 public class BankAccount {
     private double balance = 0;
     private String accountNumber;
+    private ReentrantLock bankLock;
 
     public BankAccount(double balance, String accountNumber) {
         this.balance = balance;
         this.accountNumber = accountNumber;
+        this.bankLock = new ReentrantLock();
     }
+
 
     public void deposit(double amount){
-        balance += amount;
+       boolean status = false;
+        try {
+            if (bankLock.tryLock(1000, TimeUnit.MILLISECONDS)){
+                try {
+                    balance += amount;
+                    status = true;
+                }finally {
+                    bankLock.unlock();
+                }
+            }else {
+                System.out.println("Could not get the lock. ");
+            }
+
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Transaction status = " + status);
     }
 
+
     public void withdraw(double amount){
-        balance -= amount;
+        boolean status = false;
+       try {
+           if (bankLock.tryLock(1000, TimeUnit.MILLISECONDS)){
+               try {
+                   balance -= amount;
+                   status = true;
+               }finally {
+                   bankLock.unlock();
+               }
+           }else {
+               System.out.println("Could not get the lock");
+           }
+       }catch (InterruptedException e){
+           System.out.println(e.getMessage());
+       }
+        System.out.println("Transaction status = " + status);
     }
 
     public double getBalance() {
         return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber(){
+        System.out.println("Account Number: " + accountNumber);
     }
 }
--- JAVA/COncurrencyChallenge/src/com/company/Main.java
+++ JAVA/COncurrencyChallenge/src/com/company/Main.java
@@ -3,14 +3,51 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    BankAccount myBank = new BankAccount(1000, "12345-678");
+	    final BankAccount myBank = new BankAccount(1000.00, "12345-678");
 	    Customer customer1 = new Customer(myBank, "Customer 1");
 	    Customer customer2 = new Customer(myBank, "Customer 2");
 
-        new Thread(customer1).start();
-        new Thread(customer2).start();
+      /*  new Thread(customer1).start();
+        new Thread(customer2).start();*/
 
+        /* One of the Solutions */
+        Thread thread1 = new Thread(){
+            @Override
+            public void run() {
+                myBank.deposit(300.00);
+                myBank.withdraw(50);
+            }
+        };
 
+        Thread thread2 = new Thread(){
+            @Override
+            public void run() {
+                myBank.deposit(300.00);
+                myBank.withdraw(50);
+            }
+        };
+
+        thread1.start();
+        thread2.start();
+
+        Thread newThread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                myBank.deposit(300.00);
+                myBank.withdraw(50);
+            }
+        });
+
+        Thread newThread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                myBank.deposit(300.00);
+                myBank.withdraw(50);
+            }
+        });
+
+        newThread1.start();
+        newThread2.start();
 
     }
 }
