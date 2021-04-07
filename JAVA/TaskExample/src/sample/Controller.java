f2e39c2e293c0a6519d877e3a6e66b6a8933dc5b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-07 17:23:17 +0200

    Task - Service

--- JAVA/TaskExample/src/sample/Controller.java
+++ JAVA/TaskExample/src/sample/Controller.java
@@ -6,6 +6,8 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -63,6 +65,27 @@ public class Controller {
         progressBar.progressProperty().bind(service.progressProperty());
         progressLabel.textProperty().bind(service.messageProperty());
         listView.itemsProperty().bind(service.valueProperty());
+/*        service.setOnRunning(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                progressBar.setVisible(true);
+                progressLabel.setVisible(true);
+            }
+        });
+
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                progressBar.setVisible(false);
+                progressLabel.setVisible(false);
+            }
+        });
+
+        progressLabel.setVisible(false);
+        progressBar.setVisible(false);*/
+
+        progressBar.visibleProperty().bind(service.runningProperty());
+        progressLabel.visibleProperty().bind(service.runningProperty());
     }
 
     @FXML
