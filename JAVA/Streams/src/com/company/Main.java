9aba5bdda2c35be03dfff347a1922e060797d0ab
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-08 20:39:10 +0200

    Lambda Streams

--- JAVA/Streams/src/com/company/Main.java
+++ JAVA/Streams/src/com/company/Main.java
@@ -9,13 +9,19 @@ public class Main {
     public static void main(String[] args) {
         List<String> bingoNumbers = Arrays.asList(
                 "G53", "G51", "I29", "N40",
-                "N36", "B6", "071", "G49", "I17",
+                "N36", "B6", "071", "G49", "I17", "g64",
                 "G50", "B12");
 
-        bingoNumbers.forEach(number -> {
-            if (number.startsWith("G")){
-                System.out.println(number);
+        List<String> gNumbers = new ArrayList<>();
+        /* Stream source must not be changed  */
+        bingoNumbers.stream().map(String::toUpperCase).filter(s -> s.startsWith("G")).sorted().forEach(System.out::println);
+ /*       bingoNumbers.forEach(number -> {
+            if (number.toUpperCase().startsWith("G")){
+                gNumbers.add(number);
+                //System.out.println(number.toUpperCase());
             }
         });
+        gNumbers.sort((s1, s2) -> s1.compareTo(s2));
+        gNumbers.forEach((s) -> System.out.println(s));*/
     }
 }
