2ad25fff59e7c7f8e2e4443cc6d653b1bf823b3c
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-08 21:50:56 +0200

    Lambda Chalanges

--- JAVA/Lambda_Challenges/src/com/company/Main.java
+++ JAVA/Lambda_Challenges/src/com/company/Main.java
@@ -1,15 +1,58 @@
 package com.company;
 
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
-        Runnable runnable = (() -> { String myString  = "Let's split this up into an array";
+        /*Q1*/
+        Runnable runnable = (() -> {
+            String myString = "Let's split this up into an array";
             String[] parts = myString.split(" ");
-            for(String s : parts){
+            for (String s : parts) {
                 System.out.println(s);
             }
         });
         new Thread(runnable).start();
+
+        /*Q2*/
+        Function<String, String> everySecondChar = (s) -> {
+            StringBuilder returnable = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if (i % 2 == 1) {
+                    returnable.append(s.charAt(i));
+                }
+            }
+            return returnable.toString();
+        };
+
+        /*Q3*/
+        Function<String, String> lambdaFunction = s -> {
+            StringBuilder returnable = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if (i % 2 == 1) {
+                    returnable.append(s.charAt(i));
+                }
+            }
+            return returnable.toString();
+        };
+
+  /*ANS*/      System.out.println(lambdaFunction.apply("1234567890"));
+
+
+ /**/   everySecondChar(lambdaFunction.apply("1234567890"));
+
+
     }
+
+    /*Q4*/
+ /*A*/   private static void everySecondChar(String apply) {
+        System.out.println(apply);
+    }
+
+ /*B*/   public static String everySecondChar(Function<String, String> func){
+        return func.apply("1234567890");
+    }
+
 
 }
