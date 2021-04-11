af88b35d55337213a8e072cf18dab8d537bc3b30
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-11 13:25:23 +0200

    DATABASE

--- JAVA/DataBases/src/com/company/Main.java
+++ JAVA/DataBases/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Test DB
+
+        try {
+           // Connection conn = DriverManager.getConnection("jdbc:sqlite:D\\databases\\testJava.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:~/dev/web50/JAVA/Databases/testJava.db");
+
+        }catch (SQLException e){
+            System.out.println("Something is wrong " + e.getMessage());
+        }
+
     }
 }
