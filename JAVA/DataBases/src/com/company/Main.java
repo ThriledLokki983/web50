10d1900ed02b6451b85076348bbab910f58faa7d
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-11 13:39:34 +0200

    DATABASE

--- JAVA/DataBases/src/com/company/Main.java
+++ JAVA/DataBases/src/com/company/Main.java
@@ -10,13 +10,12 @@ public class Main {
     public static void main(String[] args) {
 	// Test DB
 
-        try {
-           // Connection conn = DriverManager.getConnection("jdbc:sqlite:D\\databases\\testJava.db");
-            /* Using the Driver Manager */
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/ogidi/dev/web50/JAVA/Databases/testJava.db");
-            Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+        // Connection conn = DriverManager.getConnection("jdbc:sqlite:D\\databases\\testJava.db");
+        /* Using the Driver Manager */
 
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/ogidi/dev/web50/JAVA/Databases/testJava.db");
+             Statement statement = conn.createStatement()){
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
         }catch (SQLException e){
             System.out.println("Something is wrong: " + e.getMessage());
         }
