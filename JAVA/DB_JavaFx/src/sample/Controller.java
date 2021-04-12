13284f802ec39248150604dbf574ef9106ae3fa5
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-12 20:20:45 +0200

    DATABASE-MUSIC-JAVAFX

--- JAVA/DB_JavaFx/src/sample/Controller.java
+++ JAVA/DB_JavaFx/src/sample/Controller.java
@@ -3,10 +3,20 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import sample.Model.Artist;
 import sample.Model.DataSource;
 
 public class Controller {
+
+    @FXML
+    private TableView<Artist> artistTable;
+
+    public void listArtist(){
+        Task<ObservableList<Artist>> task = new GetAllArtistTask();
+        artistTable.itemsProperty().bind(task.valueProperty());
+    }
 
 
 }
