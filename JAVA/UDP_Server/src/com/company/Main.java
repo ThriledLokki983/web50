3a84c607527b8990f858003013e3e9f56b9a4e6b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-13 17:17:01 +0200

    UDP_Server

--- JAVA/UDP_Server/src/com/company/Main.java
+++ JAVA/UDP_Server/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            DatagramSocket socket = new DatagramSocket(5000);
+
+            while (true){
+                byte[] buffer = new byte[50];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                System.out.println("Text received: " + "\t" + new String(buffer));
+            }
+
+        }catch (SocketException e){
+            System.out.println("Socket Exception " + e.getLocalizedMessage());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
