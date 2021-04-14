7f9bc010dfa4133eefcfac0bce4e41dd3a69e927
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-14 19:59:06 +0200

    JavaScript Fundamentals-2

--- JAVASCRIPT/Course_Materials/02-Fundamentals-Part-2/starter/script.js
+++ JAVASCRIPT/Course_Materials/02-Fundamentals-Part-2/starter/script.js
@@ -12,5 +12,14 @@ if (hasDriversLicense) {
 
 function logger() {
   console.log("hello, world to functions");
+  console.log(56 * 56);
 }
 logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+let appleJuice = fruitProcessor(5, 2);
+console.log(appleJuice);
