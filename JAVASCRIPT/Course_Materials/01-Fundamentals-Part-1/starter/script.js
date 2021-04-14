343f5a73b7c29e07e5ea105939b4f26555c0686a
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-14 11:12:16 +0200

    JavaScript Fundamentals

--- JAVASCRIPT/Course_Materials/01-Fundamentals-Part-1/starter/script.js
+++ JAVASCRIPT/Course_Materials/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 //if (js === 'amazing') alert("Javascript is FUN!")
 
@@ -20,3 +21,67 @@ var population = 36000000;
 console.log("Country: " + country);
 console.log("Population: " + population);
 console.log("Continent: " + continent);
+*/
+
+// DATA TYPES:
+// Number: floating point numbers 
+// String: Sequence of character 
+// Boolean: Logical type that can only be true or false
+// Undefine: Value taken by a variable that is not yet defined
+// Null: ALso means an empty value
+// Symbol: Value that is unique and cannot be change 
+// BigInt: Larger integers than the number type can hold  
+/*
+true;
+console.log(true);
+console.log("-------")
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log("-------")
+// Type of 
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Gideon");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log('--------------');
+console.log(year);
+console.log(typeof year);
+
+// Challenge for
+
+let country = 'Ghana';
+let continent = 'Africa';
+let population = 36000000;
+let isIsland = false;
+let language;
+console.log('--------------')
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+*/
+
+// let age = 30;
+// age = 31; // Mutate the value of age
+
+// const birthYear = 1990; // Immutable variable - it cannot be empty
+// //const job; // will not work
+
+// lastName = "Nimoh"; // Always declare variables properly
+// console.log(lastName);
+
+// OPERATORS //
+const now = 2037
+const ageGideon = now - 1990; //
+const ageSarah = now - 2018;
+
+console.log("Gideon is: " + ageGideon, "&  Sara is: " + ageSarah);
+console.log(ageGideon * 2, ageGideon / 10, 2 ** 3)
