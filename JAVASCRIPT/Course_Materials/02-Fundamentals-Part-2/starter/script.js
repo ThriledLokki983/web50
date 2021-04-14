e88c35d8f5c21f63e0a75b2bd7e8916c44bb95f9
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-14 20:23:55 +0200

    JavaScript Fundamentals-2

--- JAVASCRIPT/Course_Materials/02-Fundamentals-Part-2/starter/script.js
+++ JAVASCRIPT/Course_Materials/02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -23,3 +24,28 @@ function fruitProcessor(apples, oranges) {
 }
 let appleJuice = fruitProcessor(5, 2);
 console.log(appleJuice);
+
+const num = Number('23');
+
+FUNCTIONS 
+
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+console.log(calcAge(1990))
+
+let calcAge1 = function (birthYear) {
+  return 2037 - birthYear;
+}
+*/
+
+let calcAge3 = birthYear => 2037 - birthYear;
+console.log(calcAge3(1990))
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  return `${firstName} retires in ${65 - (2021 - birthYear)} years`;
+}
+
+console.log(yearUntilRetirement(1990, "Gideon"));
+console.log(yearUntilRetirement(2007, "John"));
