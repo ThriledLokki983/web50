456feadc0fffed4cd56f1d558db5a84f488ecebe
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-14 19:44:59 +0200

    JavaScript Fundamentals-2

--- JAVASCRIPT/Course_Materials/02-Fundamentals-Part-2/starter/script.js
+++ JAVASCRIPT/Course_Materials/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) {
+  console.log('I can drive :D');
+}
+
+// Functions
+
+function logger() {
+  console.log("hello, world to functions");
+}
+logger();
