8cf3331dcde067d2dfd2b0191948224f085b1cd8
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-17 11:39:04 +0200

    JavaScript-DOM

--- JAVASCRIPT/Course_Materials/05-Guess-My-Number/starter/script.js
+++ JAVASCRIPT/Course_Materials/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log(document.querySelector('.label-score').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number 🎁'
+console.log(document.querySelector('.message').textContent = 'Correct Number 🎁');
+
+document.querySelector('.score').textContent = 'Scoring'
+document.querySelector('.number').textContent = '13'
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
