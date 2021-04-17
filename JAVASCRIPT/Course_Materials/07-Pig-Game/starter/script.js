22642c15249843c20dfdc723da23d18c851427a2
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-17 20:32:04 +0200

    JavaScript-DOM3

--- JAVASCRIPT/Course_Materials/07-Pig-Game/starter/script.js
+++ JAVASCRIPT/Course_Materials/07-Pig-Game/starter/script.js
@@ -12,6 +12,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// Global variables
+let scores, currentScore, activePlayer, isPlaying;
+
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  isPlaying = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -20,17 +42,12 @@ const switchPlayer = () => {
   player1El.classList.toggle("player--active");
 }
 
-
 // Starting Game conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// Global variables
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let isPlaying = true;
+
 
 // Rolling of the Dice Functionality
 btnRoll.addEventListener('click', () => {
@@ -55,7 +72,6 @@ btnHold.addEventListener("click", () => {
   if (isPlaying) {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-
     if (scores[activePlayer] >= 20) {
       isPlaying = false;
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -66,3 +82,5 @@ btnHold.addEventListener("click", () => {
     }
   }
 })
+
+btnNew.addEventListener('click', init)
