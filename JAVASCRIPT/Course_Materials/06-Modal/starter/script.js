7183596677683de29fd15077638b710c1512dc94
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-17 17:59:44 +0200

    JavaScript-DOM

--- JAVASCRIPT/Course_Materials/06-Modal/starter/script.js
+++ JAVASCRIPT/Course_Materials/06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const openModal = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const openTheModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < openModal.length; i++) {
+  openModal[i].addEventListener('click', openTheModal)
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
