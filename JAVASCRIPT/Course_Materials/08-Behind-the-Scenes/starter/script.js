c51308d595bcf40310497909e19723d4c089aae1
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-18 12:22:36 +0200

    How javascript works behind the scene

--- JAVASCRIPT/Course_Materials/08-Behind-the-Scenes/starter/script.js
+++ JAVASCRIPT/Course_Materials/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const gideon = {
+  firstName: 'Gideon',
+  year: 1990,
+
+  calcAge: function () {
+    return `You are ${2060 - this.year}`
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`)
+}
+gideon.greet();
+
+console.log(gideon.calcAge());
+
+const matilda = {
+  name: 'Matilda',
+  year: 2004
+}
+
+matilda.calcAge = gideon.calcAge;
+console.log(matilda.calcAge());
+
+const f = gideon.calcAge;
