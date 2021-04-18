a8c54f2ffdc9a4ccfeec5849a5d471efa4797c6c
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-18 12:49:28 +0200

    How javascript works behind the scene

--- JAVASCRIPT/Course_Materials/08-Behind-the-Scenes/starter/script.js
+++ JAVASCRIPT/Course_Materials/08-Behind-the-Scenes/starter/script.js
@@ -1,25 +1,78 @@
 'use strict';
 
+// const gideon = {
+//   firstName: 'Gideon',
+//   year: 1990,
+
+//   calcAge: function () {
+//     return `You are ${2060 - this.year}`
+
+//     const isMillenial = function () {
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+//     isMillenial();
+//   },
+
+//   greet: () => {
+//     console.log(`Hey ${this.firstName}`);
+//   }
+// }
+// gideon.greet();
+// gideon.calcAge();
+
+// console.log(gideon.calcAge());
+
+// const matilda = {
+//   name: 'Matilda',
+//   year: 2004
+// }
+
+// matilda.calcAge = gideon.calcAge;
+// console.log(matilda.calcAge());
+
+// const f = gideon.calcAge;
+
 const gideon = {
   firstName: 'Gideon',
   year: 1990,
 
   calcAge: function () {
-    return `You are ${2060 - this.year}`
+    console.log(2037 - this.year);
+
+
+    //   const self = this;
+    //   const isMillenial = function () {
+    //     console.log(self);
+    //     console.log(self.year >= 1981 && self.year <= 1996);
+    //   };
+    //   isMillenial();
+    // },
+
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
 
-  greet: () => console.log(`Hey ${this.firstName}`)
+  greet: () => {
+    console.log(`Hey ${this.firstName}`);
+  }
 }
 gideon.greet();
+gideon.calcAge();
 
-console.log(gideon.calcAge());
+// Argument keyword is
 
-const matilda = {
-  name: 'Matilda',
-  year: 2004
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 }
+console.log(addExpr(2, 5, 9, 8, 3));
 
-matilda.calcAge = gideon.calcAge;
-console.log(matilda.calcAge());
-
-const f = gideon.calcAge;
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+// addArrow(3, 5, 6, 9);
