f8830b662b368c7176b9c1142f00afe2d5bb9270
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-19 13:13:57 +0200

    Sets

---
+++ JAVASCRIPT/Course_Materials/09-Data-Structures-Operators/starter/sets.js
@@ -0,0 +1,56 @@
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function ({
+    starterIndex,
+    mainIndex,
+    time,
+    address
+  }) {
+    console.log(`Order Received: ${this.starterMenu[starterIndex]} - Starter
+    ${this.mainMenu[mainIndex]} - Main
+    will be delivered to ${address}
+    at ${time}`);
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
+};
+
+// Duplicates will be ignored-- Thus, all items are grouped -- it is iterable
+// Order of elements are irrelevent,
+const orderSet = new Set(['Pasta', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
+console.log(orderSet);
+console.log(new Set("Gideon").size) // Lenght in Array
+console.log(new Set("Gideon").has('e')); // Includes in Array
+console.log(new Set("Gideon").add('a')) // Push in Array
+console.log(new Set("Gideon").delete('a')) // Pop in Array
