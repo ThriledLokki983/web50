14e10dbe5d62087e43f1fa534e454b8ec8c00ac1
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-19 11:41:43 +0200

    Object-Loop

---
+++ JAVASCRIPT/Course_Materials/09-Data-Structures-Operators/starter/looping-objects.js
@@ -0,0 +1,54 @@
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function ({
+    starterIndex,
+    mainIndex,
+    time,
+    address
+  }) {
+    console.log(`Order Received: ${this.starterMenu[starterIndex]} - Starter
+    ${this.mainMenu[mainIndex]} - Main
+    will be delivered to ${address}
+    at ${time}`);
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
+};
+
+
+// Loopiong over an Array
+for(let day of Object.keys(restaurant.openingHours)){
+  console.log(day)
+}
