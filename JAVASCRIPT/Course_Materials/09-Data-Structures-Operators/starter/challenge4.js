7509835835b62e3910dc10d860e6067fbc27f5ad
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-20 10:53:48 +0200

    Challenge4

--- JAVASCRIPT/Course_Materials/09-Data-Structures-Operators/starter/challenge4.js
+++ JAVASCRIPT/Course_Materials/09-Data-Structures-Operators/starter/challenge4.js
@@ -79,5 +79,30 @@ button.addEventListener('click', () => {
 	// 	}
 	// }
 
-
 });
+
+// Data needed for a later exercise
+// Data needed for a later exercise
+const flights =
+	'_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (let flight of flights.split('+')) {
+	const [type, from, to, time] = flight.split(';');
+	const output = `${type.startsWith('_Delayed') ? '🛑' : ''}${type.replaceAll('_', ' ')} ${getCode(from)} -> ${getCode(to)} (${time.replace(':', 'h')})`.padStart(40);
+	console.log(output);
+}
+
+// for (let [index, flight] of raw.entries()) {
+// 	flight = flight.split(';')
+// 	let str = flight[0].replace(flight[0][0], '');
+// 	for (let n of flight) {
+// 		// console.log(n.split('_'))
+// 	}
+// 	// console.log(...flight);
+// 	for (let n of flight) {
+// 		n = n.replace(n[0], '').split('_')
+// 		// console.log(n);
+// // 	}
+// }
