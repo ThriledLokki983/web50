bd1a71371d7bf7f084b7933c0b89b3c42adc5ec5
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-20 18:45:04 +0200

    IIFE & Closures

--- JAVASCRIPT/Course_Materials/10-Functions/starter/invoke.js
+++ JAVASCRIPT/Course_Materials/10-Functions/starter/invoke.js
@@ -14,3 +14,28 @@ runOnce();
 
 // Similar for Arraow Functions
 (() => console.log(`Similar but just for Arrow Functions`))();
+
+// Importance
+// - All data/variable defined within a specific scope is private/encapsulated to that scope area
+
+console.log("=============================================================")
+
+
+// CLOSURES -- makes that returned function remember all variables at the birthplace of that function
+// A function has access to the variable environment (VE) of the execution context inwhich it was created
+// CLosure ==> VE attached to the function, exactly as it was at the time and place the function was created
+let passengerCount = 10;
+
+const secureBooking = function () {
+	let passengerCount = 5;
+
+	return function () {
+		passengerCount++;
+		console.log(`${passengerCount}: Passengers`)
+	}
+}
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
