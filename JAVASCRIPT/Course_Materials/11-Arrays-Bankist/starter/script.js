8d2639b85e353b9524cae645c040c02a982eefd9
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-21 12:35:15 +0200

    Challenge1

--- JAVASCRIPT/Course_Materials/11-Arrays-Bankist/starter/script.js
+++ JAVASCRIPT/Course_Materials/11-Arrays-Bankist/starter/script.js
@@ -61,67 +61,19 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-for (let move of movements) {
-	move > 0 ? console.log(`You deposited ${move}`) : console.log(`You withdrew ${Math.abs(move)}`);
+const displayTransactions = function (transactions) { // pass the data into the function rather than using directly a global variable
+	containerMovements.innerHTML = '';
+	transactions.forEach(function (mov, i) {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
+		const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i+1} ${type} </div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+		containerMovements.insertAdjacentHTML('afterbegin', html) // afterbegin & beforeend
+	})
 }
-console.log('================ FOR-EACH METHOD ====================')
+displayTransactions(account1.movements);
 
-// Using the foreach Method -- it uses an Anonymous function as a callback function to tell the higher order function what to do
-movements.forEach(function (move) {
-	move > 0 ? console.log(`You deposited ${move}`) : console.log(`You withdrew ${Math.abs(move)}`);
-})
-
-console.log('================ FOR-EACH METHOD + the index ====================')
-// Getting the current index == the parameter names don't matter but the ORDER does ==>
-// 1. the current element
-// 2. the current index
-// 3. the entire array we are lopping over
-movements.forEach(function (move, i, arr) {
-	move > 0 ? console.log(`Movement ${i + 1}: You deposited ${move}`) : console.log(`Movement ${i + 1}: You withdrew ${Math.abs(move)}`);
-});
-
-// When to use the forEach and when to use for of loop
-// You cannot break out from forEach loop but you can in the for-of loop
-
-console.log('================ FOR-EACH METHOD + MAPS ====================')
-// ForEach in Maps
-// 1. the current Value
-// 2. the current key
-// 3. the entire Map we are lopping over
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-currencies.forEach(function (value, key, entireMap) {
-	console.log(`${key}: ${value}`);
-})
-
-console.log('================ FOR-EACH METHOD + SETS ====================')
-// ForEach in Sets
-// 1. the current Value
-// 2. the current key == same as value
-// 3. the entire Map we are lopping over
-
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-// console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, _, set) {
-	console.log(`${value}: ${value}`);
-})
+console.log(containerMovements.innerHTML);
