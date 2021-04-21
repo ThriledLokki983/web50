4b3e3d00d58ccff6e3cfb7f470b243747913a5fe
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-21 16:32:30 +0200

    Array Methods

--- JAVASCRIPT/Course_Materials/11-Arrays-Bankist/starter/array-methods.js
+++ JAVASCRIPT/Course_Materials/11-Arrays-Bankist/starter/array-methods.js
@@ -104,10 +104,10 @@ console.log(balance2);
 const max = movements.reduce((acc, curr) => acc > curr ? acc : curr, movements[0]);
 console.log(max);
 
-// Chaining of Array Methods
-const totalDeposits =
-	movements.filter(mov => mov > 0)
+// Chaining of Array Methods -- disadvantage -- Hardto Debug
+const totalDeposits = movements
+	.filter(mov => mov > 0)
 	.map((mov) => mov * eurosToUsd)
 	.reduce((initial, current) => initial + current, 0);
 
-console.log(totalDeposits);
+console.log(Number(totalDeposits.toFixed(0)));
