da5ef05bcce8f012d54bee99dd09f9205d1a23c4
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-22 13:26:55 +0200

    Challenge3

--- JAVASCRIPT/Course_Materials/11-Arrays-Bankist/starter/challenge1.js
+++ JAVASCRIPT/Course_Materials/11-Arrays-Bankist/starter/challenge1.js
@@ -47,7 +47,7 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 console.log('================== Challeng - 2 ================')
 
-// Q-1
+// CHALLENGE 3
 const calcAverageHumanAge = function (arr) {
 	const humanAges = arr.map(curr => curr <= 2 ? curr * 2 : 16 + (curr * 4))
 		.filter((curr) => curr >= 18)
@@ -65,20 +65,88 @@ const calcAverageHumanAge1 = arr => arr
 console.log(calcAverageHumanAge1([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge1([16, 6, 10, 5, 6, 1, 4]));
 
-// // Q-2
-// const newArr = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-// const newArr1 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-// const atLeast18 = newArr.filter(curr => curr >= 18);
-// const atLeast181 = newArr1.filter(curr => curr >= 18);
-// console.log(atLeast18);
-// console.log(atLeast181);
-//
-// // Q-3
-// let sum = 0;
-// const allAdults = [...atLeast18, ...atLeast181];
-// for (let i of allAdults) {
-// 	sum += i;
-// }
-// console.log(Number((sum / allAdults.length).toFixed(0)));
-//
-// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+console.log('================== Challeng - 4 ================')
+
+//TEST Data
+const dogs = [
+	{
+		weight: 22,
+		curFood: 250,
+		owners: ['Alice', 'Bob']
+	},
+	{
+		weight: 8,
+		curFood: 200,
+		owners: ['Matilda']
+	},
+	{
+		weight: 13,
+		curFood: 275,
+		owners: ['Sarah', 'John']
+	},
+	{
+		weight: 8,
+		curFood: 340,
+		owners: ['Michiel']
+	}
+]
+
+// Loop over the array containing the dog objects and for each dog, calculate
+// the recommended food portion and add it to the object as a new array,
+
+// Q- 1
+//const newDogs = dogs.map(item => item.recFood = Number(((item.weight / 1000) ** 0.75 * 28).toFixed(2)));
+for (let dog of dogs) {
+	dog.recFood = Number(((dog.weight) ** 0.75 * 28).toFixed(0));
+}
+console.log(dogs);
+
+
+// Q - 2
+// Find Sara's dog
+for (let dog of dogs) {
+	let str = dog.curFood > dog.recFood ? ' much' : ' little';
+	dog.owners.includes('Sarah') ? console.log(`${dog.owners[0]}'s dog is eating too ${str}`) : '';
+}
+
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(dogSarah);
+
+// Q -3
+const high = item => item.curFood > item.recFood ? item.owners : '';
+const low = item => item.curFood < item.recFood ? item.owners : '';
+const ownersEatTooMuch = dogs.flatMap(high).filter((item) => item !== '');
+const ownersEatTooLittle = dogs.flatMap(low).filter((item) => item !== '');
+const ownersEatTooMuch1 = dogs.filter((item) => item.curFood > item.recFood).flatMap((item) => item.owners)
+const ownersEatTooLittle1 = dogs.filter((item) => item.curFood < item.recFood).flatMap((item) => item.owners);
+
+console.log(ownersEatTooMuch1)
+console.log(ownersEatTooMuch)
+
+console.log(ownersEatTooLittle)
+console.log(ownersEatTooLittle1)
+
+// console.log(`Owners eat too little: ${ownersEatTooLittle}`);
+
+// Q - 4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too littel`);
+
+// Q -5
+
+
+
+// Q - 6
+
+// Q -7
+
+// Q - 8
+
+
+
+
+
+
+
+
+// end of functions challenge4
