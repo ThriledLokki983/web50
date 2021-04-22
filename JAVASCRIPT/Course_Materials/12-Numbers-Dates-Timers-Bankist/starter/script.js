f8a73abe3b1ca3e6a89238067715957630b91f5c
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-22 16:05:24 +0200

    Bankist-App

--- JAVASCRIPT/Course_Materials/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ JAVASCRIPT/Course_Materials/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -23,7 +23,7 @@ const account1 = {
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
     '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-07-12T10:51:36.790Z',
   ],
 	currency: 'EUR',
 	locale: 'pt-PT', // de-DE
@@ -81,19 +81,26 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
 	containerMovements.innerHTML = '';
 
-	const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+	const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
 	movs.forEach(function (mov, i) {
 		const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+		const date = new Date(acc.movementsDates[i]);
+		const day = `${date.getDate()}`.padStart(2, 0);
+		const month = `${date.getMonth() + 1}`.padStart(2, 0);
+		const year = date.getFullYear();
+		const displayDate = `${day}/${month}/${year}`;
 
 		const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+    <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -142,7 +149,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
 	// Display movements
-	displayMovements(acc.movements);
+	displayMovements(acc);
 
 	// Display balance
 	calcDisplayBalance(acc);
@@ -154,6 +161,16 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+// FAKE LOGIN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+
+
+
+// day/Month/year
 
 btnLogin.addEventListener('click', function (e) {
 	// Prevent form from submitting
@@ -170,6 +187,15 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
 		containerApp.style.opacity = 100;
+
+		// Create the current date and time
+		const now = new Date();
+		const day = `${now.getDate()}`.padStart(2, 0);
+		const month = `${now.getMonth() + 1}`.padStart(2, 0);
+		const year = now.getFullYear();
+		const hours = `${now.getHours()}`.padStart(2, 0);
+		const minutes = `${now.getMinutes()}`.padStart(2, 0);
+		labelDate.textContent = `${day}/${month}/${year}, ${hours}:${minutes}`;
 
 		// Clear input fields
 		inputLoginUsername.value = inputLoginPin.value = '';
@@ -198,8 +224,15 @@ btnTransfer.addEventListener('click', function (e) {
 		currentAccount.movements.push(-amount);
 		receiverAcc.movements.push(amount);
 
+
+		// Add Transfer date
+		currentAccount.movementsDates.push(new Date().toISOString());
+		receiverAcc.movementsDates.push(new Date().toISOString());
+
 		// Update UI
 		updateUI(currentAccount);
+
+
 	}
 });
 
@@ -211,6 +244,9 @@ btnLoan.addEventListener('click', function (e) {
 	if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
 		// Add movement
 		currentAccount.movements.push(amount);
+
+		// Add Loan date
+		currentAccount.movementsDates.push(new Date().toISOString());
 
 		// Update UI
 		updateUI(currentAccount);
@@ -244,7 +280,7 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
 	e.preventDefault();
-	displayMovements(currentAccount.movements, !sorted);
+	displayMovements(currentAccount, !sorted);
 	sorted = !sorted;
 });
 
@@ -260,7 +296,7 @@ labelBalance.addEventListener('click', function () {
 	});
 });
 
-console.log(new Date(account1.movementsDates[0]));
+// console.log(new Date(account1.movementsDates[0]));
 
 
 /////////////////////////////////////////////////
