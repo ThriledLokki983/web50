3d214a8fb53d4852056e92f0c7db7b35fdd7a714
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-23 09:05:25 +0200

    Bankist-App

--- JAVASCRIPT/Course_Materials/12-Numbers-Dates-Timers-Bankist/starter/numbers.js
+++ JAVASCRIPT/Course_Materials/12-Numbers-Dates-Timers-Bankist/starter/numbers.js
@@ -176,13 +176,17 @@ console.log(days);
 
 
 // Internationalization API
-console.log('....')
-console.log('....')
-console.log('....')
-console.log('....')
-console.log('....')
-console.log('....')
-console.log('....')
+const num = 4986.34;
+
+const options = {
+	style: 'unit',
+	unit: 'mile-per-hour',
+}
+
+console.log('US:  ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('DE:  ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('AR:  ', new Intl.NumberFormat('ar-SY').format(num));
+console.log(navigator.language, new Intl.NumberFormat(navigator.language).format(num));
 
 
 
--- JAVASCRIPT/Course_Materials/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ JAVASCRIPT/Course_Materials/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -99,6 +99,13 @@ const formatTransactionDate = function (date, loc) {
 
 }
 
+const formatCur = function (value, locale, cur) {
+	return new Intl.NumberFormat(locale, {
+		style: 'currency',
+		currency: cur,
+	}).format(value);
+}
+
 const displayMovements = function (acc, sort = false) {
 	containerMovements.innerHTML = '';
 
@@ -112,13 +119,16 @@ const displayMovements = function (acc, sort = false) {
 
 		const displayDate = formatTransactionDate(date, acc.locale);
 
+		const formattedTrans = formatCur(mov, acc.locale, acc.currency);
+
+
 		const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
     <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__value">${formattedTrans}</div>
       </div>
     `;
 
@@ -128,19 +138,20 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
 	acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-	labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+
+	labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
 	const incomes = acc.movements
 		.filter(mov => mov > 0)
 		.reduce((acc, mov) => acc + mov, 0);
-	labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+	labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
 	const out = acc.movements
 		.filter(mov => mov < 0)
 		.reduce((acc, mov) => acc + mov, 0);
-	labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+	labelSumOut.textContent = formatCur(out, acc.locale, acc.currency);
 
 	const interest = acc.movements
 		.filter(mov => mov > 0)
@@ -150,7 +161,7 @@ const calcDisplaySummary = function (acc) {
 			return int >= 1;
 		})
 		.reduce((acc, int) => acc + int, 0);
-	labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+	labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
