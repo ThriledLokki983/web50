e592d4835a36f2dbd51579f6125d531791805986
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-23 11:33:58 +0200

    Advanced DOM Selections/Methods

---
+++ JAVASCRIPT/Course_Materials/13-Advanced-DOM-Bankist/starter/select.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// How to select ELEMENTS
+console.log(document.documentElement); // selectiong the entire html document
+console.log(document.head);
+console.log(document.body);
+
+// QUESTION:
+const header = document.querySelector('.header');
+const nodeList = Array.from(document.querySelectorAll('.section'))
+console.log(nodeList);
+
+document.getElementById('section--1')
+const btns = document.getElementsByTagName('button') // returns HTMLCollections // Live
+console.log(btns);
+
+const clas = document.getElementsByClassName('btn'); // also returns an HTMLCollections
+console.log(clas);
+
+// Creating and INserting Elements
+// .inserAdjacentHTML -- create an HTML elements
+
+const msg = document.createElement('div') // returns a DOM element
+msg.classList.add('cookie-message');
+msg.textContent = 'We use Cookies for Improving the user experience';
+msg.innerHTML = 'We use Cookies for Improving the user experience<button class="btn btn--close--cookie">Got it!</button>';
+header.prepend(msg);
+header.append(msg);
+console.log(msg);
