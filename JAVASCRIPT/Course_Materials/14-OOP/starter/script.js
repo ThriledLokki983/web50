0246cf96d8411c21d1b65e832d7a217ac7a8a836
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-24 13:17:16 +0200

    script

--- JAVASCRIPT/Course_Materials/14-OOP/starter/script.js
+++ JAVASCRIPT/Course_Materials/14-OOP/starter/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
 const Person = function (name, year) {
+  // Instance Properties
   this.name = name;
   this.year = year;
 };
 
 const per = new Person('Gideon', 1991);
+const mat = new Person('Matilda', 2003);
+const pat = new Person('Patricia', 1995);
 console.log(per);
+console.log(mat);
+console.log(pat);
+console.log(pat instanceof Person);
 
 // 1. New {} is created
 // 2. function is called, this = {}
