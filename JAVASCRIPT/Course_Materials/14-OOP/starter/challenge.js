7f182171684b3c4d918b76b3077440e30f108578
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-24 18:33:12 +0200

    Challenge

--- JAVASCRIPT/Course_Materials/14-OOP/starter/challenge.js
+++ JAVASCRIPT/Course_Materials/14-OOP/starter/challenge.js
@@ -23,3 +23,41 @@ bmw.break();
 
 mercedes.accelerate();
 mercedes.break();
+
+////////////////////////////////////////////////
+//TODO -- CHALLENGE-2
+class Car1 {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`Speed: ${this.speed}km/h`);
+  }
+
+  break () {
+    this.speed -= 5;
+    console.log(`Break: ${this.speed}km/h`);
+  }
+
+  get speedUS() {
+    return `SpeedUS: ${Math.trunc(this.speed / 1.6)}m/h`;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car1('Ford', 120);
+// const mercedes1 = new Car1('Mercedes', 95);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.break();
+ford.accelerate();
+console.log(ford.speedUS);
+ford.speedUS = 50;
+console.log(ford);
