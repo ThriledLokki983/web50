ae75975d1f6557fa6e9160aee9c4621d77868fbb
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-25 17:04:36 +0200

    Maps

--- JAVASCRIPT/Course_Materials/15-Mapty/starter/script.js
+++ JAVASCRIPT/Course_Materials/15-Mapty/starter/script.js
@@ -188,57 +188,109 @@ class App {
         .openPopup();
     }
 
-    _renderWorkout(workout){
+    _renderWorkout(workout) {
         let html = `
-        li class="workout workout--${workout.type}" data-id="${workout.id}">
-          <h2 class="workout__title">${workout.description}</h2>
-          <div class="workout__details">
-            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴🏾‍♂️'}</span>
-            <span class="workout__value">${workout.distance}</span>
-            <span class="workout__unit">km</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">⏱</span>
-            <span class="workout__value">${workout.duration}</span>
-            <span class="workout__unit">min</span>
-          </div>
-        `;
-
-        if(workout.type === 'running'){
-            html += `
+          <li class="workout workout--${workout.type}" data-id="${workout.id}">
+            <h2 class="workout__title">${workout.description}</h2>
             <div class="workout__details">
-            <span class="workout__icon">⚡️</span>
-            <span class="workout__value">${workout.pace.toFixed(1)}</span>
-            <span class="workout__unit">min/km</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">🦶🏼</span>
-            <span class="workout__value">${workout.cadense}</span>
-            <span class="workout__unit">spm</span>
-          </div>
-        </li>
-        `;
-        }
-
-        if(workout.type === 'cycling'){
-            html += `
+              <span class="workout__icon">${
+                workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+              }</span>
+              <span class="workout__value">${workout.distance}</span>
+              <span class="workout__unit">km</span>
+            </div>
             <div class="workout__details">
-            <span class="workout__icon">⚡️</span>
-            <span class="workout__value">${workout.speed.toFixed(1)}</span>
-            <span class="workout__unit">km/h</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">⛰</span>
-            <span class="workout__value">${workout.elevationGained}</span>
-            <span class="workout__unit">m</span>
-          </div>
-        </li> 
+              <span class="workout__icon">⏱</span>
+              <span class="workout__value">${workout.duration}</span>
+              <span class="workout__unit">min</span>
+            </div>
         `;
-
+    
+        if (workout.type === 'running')
+          html += `
+            <div class="workout__details">
+              <span class="workout__icon">⚡️</span>
+              <span class="workout__value">${workout.pace.toFixed(1)}</span>
+              <span class="workout__unit">min/km</span>
+            </div>
+            <div class="workout__details">
+              <span class="workout__icon">🦶🏼</span>
+              <span class="workout__value">${workout.cadense}</span>
+              <span class="workout__unit">spm</span>
+            </div>
+          </li>
+          `;
+    
+        if (workout.type === 'cycling')
+          html += `
+            <div class="workout__details">
+              <span class="workout__icon">⚡️</span>
+              <span class="workout__value">${workout.speed.toFixed(1)}</span>
+              <span class="workout__unit">km/h</span>
+            </div>
+            <div class="workout__details">
+              <span class="workout__icon">⛰</span>
+              <span class="workout__value">${workout.elevationGained}</span>
+              <span class="workout__unit">m</span>
+            </div>
+          </li>
+          `;
+    
         form.insertAdjacentHTML('afterend', html);
-        console.log(workout);
-        }
-    }
+      }
+    
+
+    // _renderWorkout(workout){
+    //     let html = `
+    //     li class="workout workout--${workout.type}" data-id="${workout.id}">
+    //       <h2 class="workout__title">${workout.description}</h2>
+    //       <div class="workout__details">
+    //         <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴🏾‍♂️'}</span>
+    //         <span class="workout__value">${workout.distance}</span>
+    //         <span class="workout__unit">km</span>
+    //       </div>
+    //       <div class="workout__details">
+    //         <span class="workout__icon">⏱</span>
+    //         <span class="workout__value">${workout.duration}</span>
+    //         <span class="workout__unit">min</span>
+    //       </div>
+    //     `;
+
+    //     if(workout.type === 'running'){
+    //         html += `
+    //         <div class="workout__details">
+    //         <span class="workout__icon">⚡️</span>
+    //         <span class="workout__value">${workout.pace.toFixed(1)}</span>
+    //         <span class="workout__unit">min/km</span>
+    //       </div>
+    //       <div class="workout__details">
+    //         <span class="workout__icon">🦶🏼</span>
+    //         <span class="workout__value">${workout.cadense}</span>
+    //         <span class="workout__unit">spm</span>
+    //       </div>
+    //     </li>`;
+    //     }
+
+    //     if(workout.type === 'cycling'){
+    //         html += `
+    //         <div class="workout__details">
+    //         <span class="workout__icon">⚡️</span>
+    //         <span class="workout__value">${workout.speed.toFixed(1)}</span>
+    //         <span class="workout__unit">km/h</span>
+    //       </div>
+    //       <div class="workout__details">
+    //         <span class="workout__icon">⛰</span>
+    //         <span class="workout__value">${workout.elevationGained}</span>
+    //         <span class="workout__unit">m</span>
+    //       </div>
+    //     </li> 
+    //     `;
+
+    //     // form.insertAdjacentHTML('afterend', html);
+    //     form.insertAdjacentHTML('afterend', html);
+    //     // console.log(workout);
+    //     }
+    // }
 }
 
 const app = new App();
