6d704c40c0a29035f8c1f4e786a4b7a313471890
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-27 12:12:41 +0200

    ASYNC-AWAIT

--- JAVASCRIPT/Course_Materials/16-Asynchronous/starter/async-await.js
+++ JAVASCRIPT/Course_Materials/16-Asynchronous/starter/async-await.js
@@ -21,11 +21,28 @@ const whereIam = async function () {
         const data = await res.json();
         // console.log(data);
         renderCountry(data[0]) // renderCountry in in script.js
+        return `You are in ${dataGeo.city}, ${dataGeo.country}`; // the resolved value of the promise
     } catch (err) {
-        console.log(err.message);
         renderError(`${err.message}`);
+
+        // reject promise from async function
+        throw err;
     }
 }
-whereIam();
+console.log(`1: Will get location`);
+// const city = whereIam();
+// console.log(city);
+// whereIam()
+//     .then(city => console.log(`2: ${city}`))
+//     .catch(err => console.log(`2: ${err.message}`))
+//     .finally(() => console.log(`3: Finished getting location`));
 
-console.log("called first")
+(async function () {
+    try {
+        const city = await whereIam();
+        console.log(`2: ${city}`);
+    } catch (err) {
+        console.log(`2: ${err.message}`);
+    }
+    console.log(`3: Finished getting location`);
+})();
