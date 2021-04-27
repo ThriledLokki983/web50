aa61f3e27f55e66f9f6da9348d87b28ecc554ff0
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-27 11:38:40 +0200

    AJAX-Call-New

--- JAVASCRIPT/Course_Materials/16-Asynchronous/starter/async-await.js
+++ JAVASCRIPT/Course_Materials/16-Asynchronous/starter/async-await.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const whereIam = async function (country) {
-    await fetch(`https://restcountries.eu/rest/v2/name/${country}`);
+// only about consuming promises
+const whereIam = async function () {
+    // fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => console.log(res));
+
+    // Geolocation
+    const pos = await getPosition();
+    const {
+        latitude: lat,
+        longitude: lng
+    } = pos.coords;
+
+    // Reverse Geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    const dataGeo = await resGeo.json();
+    // console.log(dataGeo);
+
+    // Country data
+    const res = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
+    const data = await res.json();
+    // console.log(data);
+    renderCountry(data[0]) // renderCountry in in script.js
 }
+whereIam();
+console.log("called first")
