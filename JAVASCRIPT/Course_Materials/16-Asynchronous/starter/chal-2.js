4f572fc18b621d3b737da2db17cc3494217b2112
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-27 11:20:59 +0200

    AJAX-Call-New

--- JAVASCRIPT/Course_Materials/16-Asynchronous/starter/chal-2.js
+++ JAVASCRIPT/Course_Materials/16-Asynchronous/starter/chal-2.js
@@ -1,29 +1,87 @@
-console.log('CHALLENG 2');
-console.log('===================================');
-imageContainer = document.querySelector('.images');
-// Create a func 'createImage
+// console.log('CHALLENG 2');
+// console.log('===================================');
+// imageContainer = document.querySelector('.images');
+// // Create a func 'createImage
+
+// const createImage = function (imgPath) {
+//     return new Promise(function (resolve, reject) {
+//         let el = document.createElement('img');
+//         el.src = imgPath;
+
+//         el.addEventListener('load', function () {
+//             imageContainer.append(el);
+//             resolve(el);
+//         })
+
+//         el.addEventListener('error', function () {
+//             reject(new Error('Could not load image'))
+//         })
+//     });
+// }
+
+// // createImage('/img/img-1.jpg').then(res => console.log('Success')).catch(err => console.error(err));
+// // createImage('/Users/ogidi/Desktop/img/image2.jpeg');
+
+// const wait = function (section) {
+//     return new Promise(resolve => setTimeout(resolve, section * 1000));
+// }
+
+// let image;
+// createImage('/img/img-1.jpg')
+//     .then(img => {
+//         image = img;
+//         return wait(4);
+//     }).then(() => {
+//         image.style.display = 'none';
+//         return createImage('/img/img-2.jpg');
+//     }).then((img) => {
+//         image = img;
+//         return wait(4);
+//     }).then(() => {
+//         image.style.display = 'none';
+//         return createImage('/img/img-3.jpg')
+//     })
+
+
+const imgContainer = document.querySelector('.images')
 
 const createImage = function (imgPath) {
     return new Promise(function (resolve, reject) {
-        let el = document.createElement('img');
-        el.src = imgPath;
+        const img = document.createElement('img');
+        img.src = imgPath;
 
-        el.addEventListener('load', function () {
-            imageContainer.append(el);
-            resolve(el);
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
         })
 
-        el.addEventListener('error', function () {
-            reject(new Error('Could not load image'))
+        img.addEventListener('error', function () {
+            reject(new Error("Image not found"));
         })
-    });
+    })
 }
-
-// createImage('/img/img-1.jpg').then(res => console.log('Success')).catch(err => console.error(err));
-// createImage('/Users/ogidi/Desktop/img/image2.jpeg');
 
 const wait = function (section) {
     return new Promise(resolve => setTimeout(resolve, section * 1000));
 }
 
-wait(2).then(res => createImage('/img/img-1.jpg')).catch(err => console.log(err));
+let theImage;
+
+createImage('/img/img-1.jpg').then(res => {
+        console.log('image 1 loaded');
+        theImage = res;
+        return wait(2); // the next then don't have any argument bcos wait does not return any resolved value
+    }).then(() => {
+        theImage.style.display = 'none';
+        return createImage('/img/img-2.jpg');
+    }).then(res => {
+        theImage = res;
+        return wait(2);
+    }).then(() => {
+        theImage.style.display = 'none';
+        return createImage('/img/img-3.jpg');
+    }).then(res => {
+        theImage = res;
+        return wait(2);
+    })
+    .catch(err => console.error(err));
