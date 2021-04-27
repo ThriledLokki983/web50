d1a2da2c53617c6e4d114ecb3d73c32d9ceef133
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-04-27 11:51:50 +0200

    ASYNC-AWAIT

--- JAVASCRIPT/Course_Materials/16-Asynchronous/starter/async-await.js
+++ JAVASCRIPT/Course_Materials/16-Asynchronous/starter/async-await.js
@@ -2,25 +2,34 @@
 
 // only about consuming promises
 const whereIam = async function () {
-    // fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => console.log(res));
+    try { // Geolocation
+        const pos = await getPosition();
+        const {
+            latitude: lat,
+            longitude: lng
+        } = pos.coords;
 
-    // Geolocation
-    const pos = await getPosition();
-    const {
-        latitude: lat,
-        longitude: lng
-    } = pos.coords;
+        // Reverse Geocoding
+        const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+        if (!resGeo.ok) throw new Error(`Problem getting location data`);
+        const dataGeo = await resGeo.json();
+        // console.log(dataGeo);
 
-    // Reverse Geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    const dataGeo = await resGeo.json();
-    // console.log(dataGeo);
-
-    // Country data
-    const res = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
-    const data = await res.json();
-    // console.log(data);
-    renderCountry(data[0]) // renderCountry in in script.js
+        // Country data
+        const res = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
+        if (!res.ok) throw new Error(`Problem getting country`);
+        const data = await res.json();
+        // console.log(data);
+        renderCountry(data[0]) // renderCountry in in script.js
+    } catch (err) {
+        console.log(err.message);
+        renderError(`${err.message}`);
+    }
 }
+whereIam();
+whereIam();
+whereIam();
+whereIam();
+whereIam();
 whereIam();
 console.log("called first")
