4963e941fb33b84b77d59b7395ffa40a1ac769c8
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-05-02 20:23:21 +0200

    Forkify-App

--- JAVASCRIPT/Course_Materials/18-forkify/starter/src/js/controller.js
+++ JAVASCRIPT/Course_Materials/18-forkify/starter/src/js/controller.js
@@ -141,3 +141,18 @@ const init = function () {
   addRecipeView._addHandlerUpload(controlAddRecipe);
 };
 init();
+
+
+/**
+ * IMPROVEMENTS: (EASY)
+ * Display number of pages between the pagination buttons
+ * Ability to sort search results by duration or number of ingredients
+ * Perform ingredient validation in view, before submitting the form
+ * Improve recipe ingredient input: Separate in multiple fields and laoow more that 6 ingredients
+ * 
+ * IMPROVEMENTS: (HARD)
+ * Shopping list feature: button on recipe to add ingredients to a list;
+ * Weekly meal planning feature: assign recipes to the next 7 days and show on a weekly calendar
+ * Get nutrition data on each ingredient from spoonacular API (https://spoonacular.com/food-api) and calculate calories of recipe.
+ * **Possibilities are endless**
+ */
