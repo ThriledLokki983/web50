170e0d8e5bec60673435bcc9ed1ecbba9b5f928b
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-05-02 16:45:28 +0200

    Forkify-App

--- JAVASCRIPT/Course_Materials/18-forkify/starter/src/js/view/bookmarksView.js
+++ JAVASCRIPT/Course_Materials/18-forkify/starter/src/js/view/bookmarksView.js
@@ -1,29 +1,14 @@
 import View from './View.js'
 import icons from 'url:../../img/icons.svg'; // Parcel 2
+import previewView from './previewView.js'
 
 class BookmarksView extends View {
     _parentElement = document.querySelector('.bookmarks__list');
-    _errorMessage = 'No bookmarks yet. Please add one!'
+    _errorMessage = 'No bookmarks yet. Please add one :-)'
     _successMessage = '';
 
     _generateMarkup() {
-        return this._data.map(rec => this._generateMarkupPreview(rec)).join('');
-    }
-    _generateMarkupPreview(result) {
-      const id = window.location.hash.slice(1);
-
-        return `
-        <li class="preview">
-        <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>`;
+        return this._data.map(bookmark => previewView.render(bookmark, false)).join('');
     }
 }
 
--- JAVASCRIPT/Course_Materials/18-forkify/starter/src/js/view/resultView.js
+++ JAVASCRIPT/Course_Materials/18-forkify/starter/src/js/view/resultView.js
@@ -1,5 +1,6 @@
 import View from './View.js'
 import icons from 'url:../../img/icons.svg'; // Parcel 2
+import previewView from './previewView.js'
 
 class ResultView extends View {
     _parentElement = document.querySelector('.results');
@@ -7,24 +8,8 @@ class ResultView extends View {
     _successMessage = '';
 
     _generateMarkup() {
-        return this._data.map(rec => this._generateMarkupPreview(rec)).join('');
-    }
-    _generateMarkupPreview(result) {
-      const id = window.location.hash.slice(1);
-
-        return `
-        <li class="preview">
-        <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>`;
-    }
+      return this._data.map(result => previewView.render(result, false)).join('');
+  }
 }
 
 export default new ResultView();
