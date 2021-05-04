bc46a185481305361673000d18b1a26ecb4a3287
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-04 17:36:35 +0200

    Creating Servers

--- NODE_JS/node_farm/starter/server.js
+++ NODE_JS/node_farm/starter/server.js
@@ -1,6 +1,12 @@
 const http = require("http");
 
 // Create a server
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+	console.log(req);
 	res.end("Hello from the server!");
+});
+
+// Listening to incoming request
+server.listen(8000, "127.0.0.1", () => {
+	console.log("Server is listening to requests on port 8000:");
 });
