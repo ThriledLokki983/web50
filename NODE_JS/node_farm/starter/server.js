9431ef10b3ade71f57876e57db51348b3f43845e
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-04 17:51:24 +0200

    Creating Servers

--- NODE_JS/node_farm/starter/server.js
+++ NODE_JS/node_farm/starter/server.js
@@ -1,9 +1,21 @@
 const http = require("http");
+const url = require("url");
 
 // Create a server
 const server = http.createServer((req, res) => {
-	console.log(req);
-	res.end("Hello from the server!");
+	// console.log(req.url);
+	const pathName = req.url;
+	if (pathName === "/overview" || pathName === "/") {
+		res.end("This is the overview page");
+	} else if (pathName === "/products") {
+		res.end("This is the Products page");
+	} else {
+		res.writeHead(404, {
+			"Content-type": "text/html",
+			"my-own-header": "Hello World",
+		});
+		res.end("<h1>Page not found!</h1>");
+	}
 });
 
 // Listening to incoming request
