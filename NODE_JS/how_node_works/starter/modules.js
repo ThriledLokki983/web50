2e7015e72ae0d49f43837cb63d153adc4ebce983
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-06 15:09:44 +0200

    Modules exports

--- NODE_JS/how_node_works/starter/modules.js
+++ NODE_JS/how_node_works/starter/modules.js
@@ -1,1 +1,16 @@
-console.log(arguments);
+// console.log(arguments);
+// console.log(require("module").wrapper);
+
+// Module.exports
+const C = require("./test-module1");
+
+const calc1 = new C();
+console.log(calc1.add(2, 6));
+
+// exports
+const calc2 = require("./test-module2");
+const { add, multiply, divide } = require("./test-module2");
+console.log(calc2.add(2, 6));
+console.log(calc2.multiply(2, 6));
+
+console.log(add(1, 2));
