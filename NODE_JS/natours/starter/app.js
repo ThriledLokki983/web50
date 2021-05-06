5e96339953dc1ad06f435006b5b709d568def136
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-06 21:22:55 +0200

    express

--- NODE_JS/natours/starter/app.js
+++ NODE_JS/natours/starter/app.js
@@ -4,17 +4,6 @@ const fs = require('fs');
 const app = express();
 app.use(express.json()); // Middleware: this can modify the incoming request
 
-// app.get('/', (req, res) => {
-//   //   res.status(200).send('Hello from the Server side');
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the Server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint.....');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -29,14 +18,52 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done'); // always have to send something in order to finish the request/response cycle
+app.get('/api/v1/tours/:id', (req, res) => {
+  // specify optional param as :id?
+  console.log(req.params);
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  //   if (id > tours.length)
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour, // tours: tour
+    },
+  });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  //   res.send('Done'); // always have to send something in order to finish the request/response cycle
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {});
 
 const port = 4000;
 app.listen(port, () => {
   console.log(`App running on port:\t${port}`);
 });
-
-// Routing
