3404cc1beabf419f8eb4e59334367186286f139b
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-06 15:35:46 +0200

    Async

---
+++ NODE_JS/asynchronous/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+fs.readFile(`${__dirname}/starter/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  // making an http request
+  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+    if (err) return console.log(err.message);
+    console.log(res.body.message);
+
+    fs.writeFile("./starter/dog-img.txt", res.body.message, (err) => {
+      console.log("Random image is saved");
+    });
+  });
+});
