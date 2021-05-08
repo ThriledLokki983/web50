f99566e4a36d8b6ddfa5f6f9ed81a460fc1213dc
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-08 12:54:19 +0200

    Mongo-DB

--- NODE_JS/natours/starter/server.js
+++ NODE_JS/natours/starter/server.js
@@ -1,8 +1,34 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 // ENVIRONMENT VARIABLES
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Connecting to the local DB
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// }).then(() => console.log("DB Connection successful"));
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log('DB Connection successful');
+  });
 
 console.log(app.get('env')); // current environment: Development
 // console.log(process.env);
