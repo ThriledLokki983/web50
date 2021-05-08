9261c7e52c0a289fc293f6893f2fb5d0aa82c766
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-08 14:33:11 +0200

    Updating Docs in Mongoose

--- NODE_JS/natours/starter/controllers/tourController.js
+++ NODE_JS/natours/starter/controllers/tourController.js
@@ -41,6 +41,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({ _id: req.params.id })
 
     res.status(200).json({
       status: 'success',
@@ -74,13 +75,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid Data sent!',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
