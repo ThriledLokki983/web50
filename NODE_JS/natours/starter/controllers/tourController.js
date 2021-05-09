4ee9842248ad9322d30246ee33d9457ca4ba4486
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-09 08:33:12 +0200

    Model - MVC

--- NODE_JS/natours/starter/controllers/tourController.js
+++ NODE_JS/natours/starter/controllers/tourController.js
@@ -21,7 +21,19 @@ const Tour = require('./../model/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+
+    // const tours = await Tour.find();
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
 
     res.status(200).json({
       status: 'success',
