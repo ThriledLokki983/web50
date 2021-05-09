dd6d5b857c65a6e6530537625826c135f349f175
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-09 11:39:56 +0200

    Model - Aggregation-Pipeline - MongoDB

--- NODE_JS/natours/starter/eg.js
+++ NODE_JS/natours/starter/eg.js
@@ -58,3 +58,22 @@
 // }
 
 //    console.log(req.query);
+
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`Tour id is:\t${val}`);
+//   if (+req.params.id > tours.length)
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   next();
+// };
+
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price)
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or Price',
+//     });
+//   next();
+// };
