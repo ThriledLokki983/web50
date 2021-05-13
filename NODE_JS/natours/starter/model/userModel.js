49a048d426e9d981d940ed8d81d6b28c3f4478f8
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-13 12:21:08 +0200

    Managing passwords/Bcrypt hashing

--- NODE_JS/natours/starter/model/userModel.js
+++ NODE_JS/natours/starter/model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,6 +33,20 @@ const userSchema = new mongoose.Schema({
       message: 'Password do not match',
     },
   },
+});
+
+/**
+ * Only run this if password is modified
+ * Hash the password with cost/salt of 13
+ * Delete passwordConfirm Field
+ */
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 13);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
