e1e08a6af7260847e8bd3a5a4d34eb57a90627a7
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-13 13:47:15 +0200

    Login users

--- NODE_JS/natours/starter/controllers/authController.js
+++ NODE_JS/natours/starter/controllers/authController.js
@@ -1,17 +1,19 @@
 const User = require('./../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const AppError = require('./../utils/appError');
 
 /**
- * Create a Token with the JWT library and send to the client
+ * Create a Web.Token with the JWT library and send to the client
  * Login user right after signup
  */
 exports.signup = catchAsync(async (req, res, next) => {
+  const { name, email, password, passwordConfirm } = req.body;
   const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
+    name: name,
+    email: email,
+    password: password,
+    passwordConfirm: passwordConfirm,
   });
 
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
@@ -26,3 +28,24 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+/**
+ * Check if email and password exist, if user && password is correct, if everything is ok, send token to client
+ * Login user
+ */
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide an email and password', 400));
+  }
+
+  const user = User.findOne({ email: email });
+
+  const token = '';
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
--- NODE_JS/natours/starter/controllers/userController.js
+++ NODE_JS/natours/starter/controllers/userController.js
@@ -1,9 +1,17 @@
-exports.getAllUsers = (req, res) => {
+const User = require('./../model/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
   res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
