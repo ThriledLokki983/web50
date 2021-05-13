083116aa1ed1c867fd30a5881807135e75fbdc36
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-13 09:21:31 +0200

    Async error - refactoring

--- NODE_JS/natours/starter/controllers/tourController.js
+++ NODE_JS/natours/starter/controllers/tourController.js
@@ -57,6 +57,12 @@ exports.getTour = async (req, res) => {
   }
 };
 
+/**
+ *
+ * @param {Async function} fn - returns a promise and when there is an error, the catch will pass the error to the next() function, which then further passes it on to the global error handler and that's it. Magic! -- error is correctly handled
+ * @returns {Anonymous function} - can be assigned to a parameter e.g. createTour
+ *
+ */
 const catchAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => next(err));
