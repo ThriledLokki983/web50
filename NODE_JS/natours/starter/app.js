011722a5279a72877a62f5ab98a455689a7440c3
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-13 08:48:05 +0200

    Model - Error Handling

--- NODE_JS/natours/starter/app.js
+++ NODE_JS/natours/starter/app.js
@@ -23,9 +23,21 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  // 4 - params means an error handling middleware
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
--- NODE_JS/natours/starter/controllers/tourController.js
+++ NODE_JS/natours/starter/controllers/tourController.js
@@ -70,7 +70,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid Data sent!',
+      message: err,
     });
   }
 };
@@ -91,7 +91,7 @@ exports.updateTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid Data sent!',
+      message: err,
     });
   }
 };
