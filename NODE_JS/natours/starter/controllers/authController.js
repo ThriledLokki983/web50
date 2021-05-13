950c28762c43c5849cd66882dc31d8b6e1b83b91
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-13 13:23:46 +0200

    Signup users

--- NODE_JS/natours/starter/controllers/authController.js
+++ NODE_JS/natours/starter/controllers/authController.js
@@ -1,11 +1,26 @@
 const User = require('./../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
+/**
+ * Create a Token with the JWT library and send to the client
+ * Login user right after signup
+ */
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
