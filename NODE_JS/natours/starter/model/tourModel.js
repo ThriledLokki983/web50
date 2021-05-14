96646c2043521f57275552ee41f33d5fe38ab3c2
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-14 21:23:58 +0200

    Enbedding

--- NODE_JS/natours/starter/model/tourModel.js
+++ NODE_JS/natours/starter/model/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 // Mongoose - MODEL for crud
 const tourSchema = new mongoose.Schema(
@@ -106,6 +107,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true }, // these properties are not part of the DB
@@ -120,6 +122,16 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before the save() and create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/**
+ * This will fetch the user info behind the scenes for all the Array of _id in the guides
+ * Embed Users as guides
+ */
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
