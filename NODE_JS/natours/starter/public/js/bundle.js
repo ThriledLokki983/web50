c91c2dafde7143c502cefeb529506aad556bd08d
ThriledLokki983 <gnimoh001@gmail.com>
2021-05-23 08:59:58 +0200

    upload and process multiple files

--- NODE_JS/natours/starter/public/js/bundle.js
+++ NODE_JS/natours/starter/public/js/bundle.js
@@ -9030,14 +9030,20 @@ if (loginForm) {
 }
 
 if (logoutButton) logoutButton.addEventListener('click', _login.logout);
+/**
+ * programmatically creating a multipart/form-data
+ */
+
 if (userDataForm) userDataForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  var email = document.getElementById('email').value;
-  var name = document.getElementById('name').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  console.log(form); // const email = document.getElementById('email').value;
+  // const name = document.getElementById('name').value;
+
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(e) {
--- NODE_JS/natours/starter/public/js/index.js
+++ NODE_JS/natours/starter/public/js/index.js
@@ -36,14 +36,23 @@ if (loginForm) {
 
 if (logoutButton) logoutButton.addEventListener('click', logout);
 
+/**
+ * programmatically creating a multipart/form-data
+ */
 if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const name = document.getElementById('name').value;
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
 
-    updateSettings({ name, email }, 'data');
+    // const email = document.getElementById('email').value;
+    // const name = document.getElementById('name').value;
+
+    updateSettings(form, 'data');
   });
 
 if (userPasswordForm)
