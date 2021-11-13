10b9f17475a7b6e846eb5620ea109c7e158b578c
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-11-13 11:21:17 +0100

    deleted secret

--- projects/2021/project0/project0/settings.py
+++ projects/2021/project0/project0/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mfx0r%nyv8=i7d)n2x!^env-l!@q72c^(=r*04rit8zy*oyozk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
